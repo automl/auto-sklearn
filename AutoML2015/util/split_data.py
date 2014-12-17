f1c5e54f9b6ff50b129d75f64d4e794506a3bb3d
kleinaa <kleinaa@informatik.uni-freiburg.de>
2014-12-17 11:08:43 +0100

    bugfix split_data

--- AutoML2015/util/split_data.py
+++ AutoML2015/util/split_data.py
@@ -1,7 +1,12 @@
+import numpy as np
+
+from sklearn.cross_validation import train_test_split
+
+
 def split_data(X, Y, folds, cv=False, shuffle=True):
-   
-    if fold >= folds:
-        raise ValueError((fold, folds))
+
+    #if fold >= folds:
+    #    raise ValueError((fold, folds))
     if X.shape[0] != Y.shape[0]:
         raise ValueError("The first dimension of the X and Y array must "
                          "be equal.")
@@ -12,15 +17,11 @@ def split_data(X, Y, folds, cv=False, shuffle=True):
         rs.shuffle(indices)
         Y = Y[indices]
 
-
-    X_train, X_valid, Y_train, Y_valid = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.33, random_state=42)
 #     if(cv):
 #         kf = StratifiedKFold(Y_train, n_folds=folds, indices=True)
 #         for train_index, test_index in skf:
 #             X_train, X_test = X_train[train_index], X[test_index]
 #             y_train, y_test = y[train_index], y[test_index]
-#         
-        
+
     return X_train, X_valid, Y_train, Y_valid
-        
-    
