e057a99d45cde95cdb3e52c185846b57ff1f86b0
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2014-12-17 11:03:27 +0100

    minor

--- test/test_StopWatch.py
+++ test/test_StopWatch.py
@@ -13,13 +13,12 @@ import AutoML2015.util.Stopwatch
 
 class Test(unittest.TestCase):
 
-    def test_Stopwatch_overhead(self):
+    def test_stopwatch_overhead(self):
         # CPU overhead
         start = time.clock()
         watch = AutoML2015.util.Stopwatch.StopWatch()
         for i in range(1, 100000):
             watch.start_task("task_%d" % i)
-
             watch.stop_task("task_%d" % i)
         stop = time.clock()
         dur = stop - start
@@ -35,8 +34,8 @@ class Test(unittest.TestCase):
         stop = time.time()
         dur = stop - start
         wall_overhead = dur - watch.wall_sum()
+        
         self.assertLess(wall_overhead, 0.5)
-
         self.assertLess(cpu_overhead, wall_overhead)
 
 if __name__ == "__main__":
