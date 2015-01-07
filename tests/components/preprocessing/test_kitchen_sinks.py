cee534f61442e829f26cdf388c5e9357381e993a
Jost Tobias Springenberg <springj@informatik.uni-freiburg.de>
2015-01-07 20:28:55 +0100

    remove my random kitchen sink implementation and wrap random kitchen sinks as implemented in scikit

---
+++ tests/components/preprocessing/test_kitchen_sinks.py
@@ -0,0 +1,12 @@
+import unittest
+
+from AutoSklearn.components.preprocessing.kitchen_sinks import RandomKitchenSinks
+from AutoSklearn.util import _test_preprocessing
+
+
+class PCAComponentTest(unittest.TestCase):
+    def test_default_configuration(self):
+        transformation, original = _test_preprocessing(RandomKitchenSinks)
+        self.assertEqual(transformation.shape[0], original.shape[0])
+        self.assertEqual(transformation.shape[1], 100)
+        self.assertFalse((transformation == 0).all())
