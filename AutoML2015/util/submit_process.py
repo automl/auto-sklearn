16d088bec76a20c65a453564670dcefde9be042e
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-01-08 17:23:07 +0100

    use Popen instead of system.call

--- AutoML2015/util/submit_process.py
+++ AutoML2015/util/submit_process.py
@@ -1,19 +1,41 @@
-import time
+import shlex
 import os
+import subprocess
+
+
+def submit_call(call):
+    print "Calling: " + call
+    call = shlex.split(call)
+    try:
+        proc = subprocess.Popen(call, stdout=open(os.devnull, 'w'))
+        proc_id = proc.pid()
+    except OSError as e:
+        print e
+        return -1
+    return proc_id
 
 
 def get_algo_exec():
     # Create call to autosklearn
-    call = "CALL_AUTOSKLEARN"
+    path_to_wrapper = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    wrapper_exec = os.path.join(path_to_wrapper, "run_config_evaluation.py")
+    if not os.path.exists(wrapper_exec):
+        call = '"python run_config_evaluation.py"'
+    else:
+        call = '"python %s"' % wrapper_exec
     return call
 
 
-def run_smac(tmp_dir, dataset_name, searchspace, instance_file, limit):
+def run_smac(tmp_dir, searchspace, instance_file, limit):
+    if limit <= 0:
+        # It makes no sense to start building ensembles
+        return
     # Bad hack to find smac
     call = os.path.join("smac")
     call = " ".join([call, '--numRun', '2147483647',
                     '--cli-log-all-calls false',
                     '--cutoffTime', '2147483647',
+                    '--wallclock-limit', '10',
                     '--intraInstanceObj', 'MEAN',
                     '--runObj', 'QUALITY',
                     '--algoExec',  get_algo_exec(),
@@ -31,13 +53,18 @@ def run_smac(tmp_dir, dataset_name, searchspace, instance_file, limit):
                     '-p', os.path.abspath(searchspace),
                     '--execDir', tmp_dir,
                     '--instances', instance_file])
-    os.system(call)
-    time.sleep(2)
-    return limit
+    pid = submit_call(call)
+    return pid
 
 
-def run_ensemble_builder(tmp_dir, dataset_name, task_type, metric, limit):
-    time.sleep(2)
-    os.system(" ".join(["python ./ensembles.py", tmp_dir, dataset_name,
-                        data_dir, task_type, metric, limit])
-    return limit
+def run_ensemble_builder(tmp_dir, dataset_name, task_type, metric, limit, output_dir):
+    if limit <= 0:
+        # It makes no sense to start building ensembles
+        return
+    path_to_wrapper = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    wrapper_exec = os.path.join(path_to_wrapper, "ensembles.py")
+    call = " ".join(["python", wrapper_exec, tmp_dir, dataset_name,
+                        task_type, metric, str(limit), output_dir])
+    pid = submit_call(call)
+    return pid
+
