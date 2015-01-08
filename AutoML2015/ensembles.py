c85a4b39c9359f3c219deef3279d06e2e1733937
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-01-08 17:23:50 +0100

    decrease #calls to ensemble builder

--- AutoML2015/ensembles.py
+++ AutoML2015/ensembles.py
@@ -8,6 +8,7 @@ import os
 import sys
 import cma
 import numpy as np
+import time
 
 from AutoML2015.data import data_io
 from AutoML2015.models import evaluate
@@ -65,9 +66,9 @@ def ensemble_prediction(all_predictions, weights):
     return all_predictions.mean(axis=0)
 
 
-def main(predictions_dir, basename, task_type, metric, limit):
+def main(predictions_dir, basename, task_type, metric, limit, output_dir):
     index_run = 0
-
+    current_num_models = 0
     watch = AutoML2015.util.Stopwatch.StopWatch()
     watch.start_task("ensemble_builder")
     used_time = 0
@@ -81,7 +82,26 @@ def main(predictions_dir, basename, task_type, metric, limit):
         #=== Load the predictions from the models
         all_predictions_train = []
         dir_ensemble = os.path.join(predictions_dir, "predictions_ensemble/")
-        for f in os.listdir(dir_ensemble):
+        dir_valid = os.path.join(predictions_dir, "predictions_valid/")
+        if not os.path.isdir(dir_ensemble):
+            # prediction directory does not exist
+            time.sleep(2)
+            continue
+
+        dir_ensemble_list = os.listdir(dir_ensemble)
+        dir_valid_list = os.listdir(dir_valid)
+
+        if len(dir_ensemble_list) != len(dir_valid_list):
+            # Directories are inconsistent
+            time.sleep(2)
+            continue
+
+        if len(dir_ensemble_list) <= current_num_models:
+            # Nothing has changed since the last time
+            time.sleep(2)
+            continue
+
+        for f in dir_ensemble_list:
             predictions = np.load(os.path.join(dir_ensemble, f))
             all_predictions_train.append(predictions)
 
@@ -90,16 +110,15 @@ def main(predictions_dir, basename, task_type, metric, limit):
                                     true_labels, task_type, metric)
 
         all_predictions_valid = []
-        dir_valid = os.path.join(predictions_dir, "predictions_valid/")
-        for f in os.listdir(dir_valid):
+        for f in dir_valid_list:
             predictions = np.load(os.path.join(dir_valid, f))
             all_predictions_valid.append(predictions)
 
         #=== Compute the ensemble predictions for the valid data
-        Y_test = ensemble_prediction(np.array(all_predictions_valid), weights,)
+        Y_valid = ensemble_prediction(np.array(all_predictions_valid), weights,)
 
-        filename_test = basename + '_valid_' + str(index_run) + '.predict'
-        data_io.write(os.path.join(predictions_dir, filename_test), Y_test)
+        filename_test = os.path.join(output_dir, basename + '_valid_' + str(index_run) + '.predict')
+        data_io.write(os.path.join(predictions_dir, filename_test), Y_valid)
 
         all_predictions_test = []
         dir_test = os.path.join(predictions_dir, "predictions_test/")
@@ -110,11 +129,15 @@ def main(predictions_dir, basename, task_type, metric, limit):
         #=== Compute the ensemble predictions for the test data
         Y_test = ensemble_prediction(np.array(all_predictions_test), weights)
 
-        filename_test = basename + '_test_' + str(index_run) + '.predict'
+        filename_test = os.path.join(output_dir, basename + '_test_' + str(index_run) + '.predict')
         data_io.write(os.path.join(predictions_dir, filename_test), Y_test)
         index_run += 1
         used_time = watch.wall_elapsed("ensemble_builder")
 
+        current_num_models = len(dir_ensemble_list)
+
 if __name__ == "__main__":
     main(predictions_dir=sys.argv[1], basename=sys.argv[2],
-         task_type=sys.argv[3], metric=sys.argv[4], limit=float(sys.argv[5]))
+         task_type=sys.argv[3], metric=sys.argv[4], limit=float(sys.argv[5]),
+         output_dir=sys.argv[6])
+
