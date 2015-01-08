d4dc18efeb2f258d17239986b518b237c788013d
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-01-08 09:22:05 +0100

    add test for random forest regression

---
+++ tests/components/regression/test_random_forests.py
@@ -0,0 +1,16 @@
+import unittest
+
+from AutoSklearn.components.regression.random_forest import RandomForest
+from AutoSklearn.util import _test_regressor
+
+import sklearn.metrics
+
+
+class RandomForestComponentTest(unittest.TestCase):
+    def test_default_configuration(self):
+        for i in range(10):
+
+            predictions, targets = _test_regressor(RandomForest,
+                                                   dataset='diabetes')
+            self.assertAlmostEqual(0.30805962106685625,
+                sklearn.metrics.r2_score(y_true=targets, y_pred=predictions))
