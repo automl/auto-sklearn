0e3f34a2f517987f8efb53987f3ca1ee1cbe61cb
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-01-10 11:53:15 +0100

    manually undo changes made to support sklearn 0.16dev

--- AutoSklearn/implementations/OneHotEncoder.py
+++ AutoSklearn/implementations/OneHotEncoder.py
@@ -3,10 +3,8 @@ from scipy import sparse
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.externals import six
-#from sklearn.utils import check_arrays
-#from sklearn.utils import atleast2d_or_csc, safe_asarray
-from ..sklearn_backward_validation import check_arrays
-from ..sklearn_backward_validation import atleast2d_or_csc, safe_asarray
+from sklearn.utils import check_arrays
+from sklearn.utils import atleast2d_or_csc, safe_asarray
 
 zip = six.moves.zip
 map = six.moves.map
