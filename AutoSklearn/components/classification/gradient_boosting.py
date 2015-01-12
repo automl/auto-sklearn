Fix tree-based max_features calculation

AutoSklearn/components/classification/gradient_boosting.py
@@ -58,7 +58,8 @@ class GradientBoostingClassifier(AutoSklearnClassificationAlgorithm):
     def fit(self, X, Y):
         num_features = X.shape[1]
         max_features = float(self.max_features) * (np.log(num_features) + 1)
-        max_features = min(0.5, max_features)
+        # Use at most half of the features
+        max_features = max(1, min(int(X.shape[1] / 2), max_features))
         self.estimator = sklearn.ensemble.GradientBoostingClassifier(
             learning_rate=self.learning_rate,
             n_estimators=0,

AutoSklearn/components/classification/random_forest.py
@@ -38,7 +38,8 @@ class RandomForest(AutoSklearnClassificationAlgorithm):
         if self.max_features not in ("sqrt", "log2", "auto"):
             num_features = X.shape[1]
             max_features = int(float(self.max_features) * (np.log(num_features) + 1))
-            max_features = min(0.5, max_features)
+            # Use at most half of the features
+            max_features = max(1, min(int(X.shape[1] / 2), max_features))
         if self.bootstrap == "True":
             self.bootstrap = True
         else: