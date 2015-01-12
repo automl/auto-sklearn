44cb2c59905e6358a75097fe0c24dc3fd11378a8
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-01-12 23:24:39 +0100

    add support vector regression

--- tests/components/regression/test_support_vector_regression.py
+++ tests/components/regression/test_support_vector_regression.py
@@ -1,1 +1,20 @@
-__author__ = 'eggenspk'
+import unittest
+
+from AutoSklearn.components.regression.support_vector_regression import SupportVectorRegression
+from AutoSklearn.util import _test_regressor
+
+
+import sklearn.metrics
+
+
+class SupportVectorComponentTest(unittest.TestCase):
+
+    def test_default_configuration(self):
+
+        for i in range(10):
+            predictions, targets = _test_regressor(SupportVectorRegression,
+                                                   dataset='boston')
+            print predictions
+            print targets
+            self.assertAlmostEqual(-0.070779979927571235,
+                sklearn.metrics.r2_score(y_true=targets, y_pred=predictions))
