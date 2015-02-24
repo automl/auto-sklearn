bcb0c4a2dd0ff8a72431b5249a4c06ae4bef99c7
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-02-24 09:41:39 +0100

    update setup and renaming

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 import setuptools
-import os
 from distutils.extension import Extension
 
 #for basename in ["chelper_functions", "cdata_manager"]:
 #    os.system("cython ./AutoML2015/data/%s.pyx"%basename)
 #    os.system('gcc -w -shared -fPIC -fwrapv -O2 -fno-strict-aliasing -I/usr/include/python2.7 -o AutoML2015/data/%s.so AutoML2015/data/%s.c'%(basename, basename))
 
-setuptools.setup(name="AutoML2015",
+setuptools.setup(name="AutoSklearn",
                  description="Code to participate in the AutoML 2015 challenge.",
                  version="0.1dev",
                  # When we have a package, uncomment this
-                 ext_modules=[Extension("AutoML2015.data.cdata_manager", ["AutoML2015/data/cdata_manager.c"]),
-                              Extension("AutoML2015.data.chelper_functions", ["AutoML2015/data/chelper_functions.c"])],
+                 ext_modules=[Extension("autosklearn.data.cdata_manager", ["autosklearn/data/cdata_manager.c"]),
+                              Extension("autosklearn.data.chelper_functions", ["autosklearn/data/chelper_functions.c"])],
                  packages=setuptools.find_packages(),
                  install_requires=["numpy",
                                    "pyyaml",
