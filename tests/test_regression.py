aa602e5d906b4ec3a2609db11a18f0b6a7ffb49f
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-03-13 14:57:50 +0100

    make searchspace generation more general

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,8 +1,6 @@
 __author__ = 'eggenspk'
 
 import copy
-import numpy as np
-import StringIO
 import unittest
 
 import sklearn.datasets
@@ -18,10 +16,33 @@ from ParamSklearn.components.regression_base import ParamSklearnRegressionAlgori
 from ParamSklearn.components.preprocessor_base import ParamSklearnPreprocessingAlgorithm
 import ParamSklearn.components.regression as regression_components
 import ParamSklearn.components.preprocessing as preprocessing_components
-from ParamSklearn.util import get_dataset
+from ParamSklearn.util import get_dataset, SPARSE, DENSE, PREDICTIONS
 
 
 class TestParamSKlearnRegressor(unittest.TestCase):
+
+    def test_io_dict(self):
+        regressors = regression_components._regressors
+        for c in regressors:
+            props = regressors[c].get_properties()
+            self.assertIn('input', props)
+            self.assertIn('output', props)
+            inp = props['input']
+            output = props['output']
+
+            self.assertIsInstance(inp, tuple)
+            self.assertIsInstance(output, str)
+            for i in inp:
+                self.assertIn(i, (SPARSE, DENSE))
+            self.assertEqual(output, PREDICTIONS)
+            self.assertIn('handles_regression', props)
+            self.assertTrue(props['handles_regression'])
+            self.assertIn('handles_classification', props)
+            self.assertIn('handles_multiclass', props)
+            self.assertIn('handles_multilabel', props)
+            self.assertFalse(props['handles_classification'])
+            self.assertFalse(props['handles_multiclass'])
+            self.assertFalse(props['handles_multilabel'])
 
     def test_find_regressors(self):
         regressors = regression_components._regressors
@@ -56,7 +77,7 @@ class TestParamSKlearnRegressor(unittest.TestCase):
         self.assertIsInstance(cs, ConfigurationSpace)
         conditions = cs.get_conditions()
         hyperparameters = cs.get_hyperparameters()
-        self.assertEqual(38, len(hyperparameters))
+        self.assertEqual(36, len(hyperparameters))
         self.assertEqual(len(hyperparameters) - 4, len(conditions))
 
     def test_get_hyperparameter_search_space_include_exclude_models(self):
@@ -73,7 +94,7 @@ class TestParamSKlearnRegressor(unittest.TestCase):
         cs = ParamSklearnRegressor.get_hyperparameter_search_space(
             include_preprocessors=['pca'])
         self.assertEqual(cs.get_hyperparameter('preprocessor'),
-            CategoricalHyperparameter('preprocessor', ["None", 'pca']))
+            CategoricalHyperparameter('preprocessor', ['pca', ]))
 
         cs = ParamSklearnRegressor.get_hyperparameter_search_space(
             exclude_preprocessors=['pca'])
