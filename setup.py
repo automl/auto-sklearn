6a1e876cd545ede357714c7668c41e36bab291f9
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2015-03-31 17:44:15 +0200

    Packaging: download runsolver, SMAC and metadata; add autosklearn script; add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,9 +1,99 @@
 import setuptools
+from setuptools.command.install import install
 from distutils.extension import Extension
+import os
+import shutil
+import subprocess
+import sys
+import tarfile
 
-#for basename in ["chelper_functions", "cdata_manager"]:
-#    os.system("cython ./AutoML2015/data/%s.pyx"%basename)
-#    os.system('gcc -w -shared -fPIC -fwrapv -O2 -fno-strict-aliasing -I/usr/include/python2.7 -o AutoML2015/data/%s.so AutoML2015/data/%s.c'%(basename, basename))
+import urllib
+
+SMAC_DOWNLOAD_LOCATION = "http://aad.informatik.uni-freiburg.de/~feurerm/"
+SMAC_TAR_NAME = "smac-v2.08.01-development-1.tar.gz"
+METADATA_LOCATION = "http://aad.informatik.uni-freiburg.de/~feurerm/"
+METADATA_TAR_NAME = "metadata_automl1_000.tar.gz"
+RUNSOLVER_LOCATION = "http://www.cril.univ-artois.fr/~roussel/runsolver/"
+RUNSOLVER_TAR_NAME = "runsolver-3.3.4.tar.bz2"
+DOWNLOAD_DIRECTORY = os.path.join(os.path.dirname(__file__), ".downloads")
+BINARIES_DIRECTORY = "autosklearn/binaries"
+METADATA_DIRECTORY = "autosklearn/metalearning/files"
+
+class Download(install):
+    def run(self):
+        try:
+            shutil.rmtree(DOWNLOAD_DIRECTORY)
+        except:
+            pass
+
+        try:
+            os.makedirs(DOWNLOAD_DIRECTORY)
+        except:
+            pass
+
+        for download_url, filename in [(SMAC_DOWNLOAD_LOCATION, SMAC_TAR_NAME),
+                                       (METADATA_LOCATION, METADATA_TAR_NAME),
+                                       (RUNSOLVER_LOCATION, RUNSOLVER_TAR_NAME)]:
+            # This can fail ungracefully, because having these files is
+            # crucial to AutoSklearn!
+            urllib.urlretrieve(os.path.join(download_url, filename),
+                               filename=os.path.join(DOWNLOAD_DIRECTORY, filename))
+
+            tfile = tarfile.open(os.path.join(DOWNLOAD_DIRECTORY, filename))
+            tfile.extractall(os.path.join(DOWNLOAD_DIRECTORY,
+                                          filename.replace(".tar.gz", "").
+                                          replace(".tar.bz2", "")))
+
+        # Build the runsolver
+        sys.stdout.write("Building runsolver\n")
+        cur_pwd = os.getcwd()
+        runsolver_source_path = os.path.join(DOWNLOAD_DIRECTORY,
+                                             "runsolver-3.3.4",
+                                             "runsolver", "src")
+        os.chdir(runsolver_source_path)
+        subprocess.check_call("make")
+        os.chdir(cur_pwd)
+
+        # Create a fresh binaries directory
+        try:
+            shutil.rmtree(BINARIES_DIRECTORY)
+        except:
+            pass
+
+        try:
+            os.makedirs(BINARIES_DIRECTORY)
+            with open(os.path.join(BINARIES_DIRECTORY, '__init__.py')):
+                pass
+        except:
+            pass
+
+        # Copy the runsolver into the sources so it gets copied
+        shutil.move(os.path.join(runsolver_source_path, "runsolver"),
+                    os.path.join(BINARIES_DIRECTORY, "runsolver"))
+
+        # Copy SMAC
+        shutil.move(os.path.join(DOWNLOAD_DIRECTORY,
+                                 SMAC_TAR_NAME.replace(".tar.gz", "")),
+                    BINARIES_DIRECTORY)
+
+        try:
+            shutil.rmtree(METADATA_DIRECTORY)
+        except:
+            pass
+
+        # Copy the metadata
+        shutil.move(os.path.join(DOWNLOAD_DIRECTORY,
+                                 METADATA_TAR_NAME.replace(".tar.gz", ""),
+                                 "files"),
+                    METADATA_DIRECTORY)
+
+        # TODO: Normally one wants to call run(self), but this runs distutils and ignores install_requirements for unknown reasons
+        # if anyone knows a better way, feel free to change
+        install.do_egg_install(self)
+
+        shutil.rmtree(os.path.join(METADATA_DIRECTORY))
+        shutil.rmtree(BINARIES_DIRECTORY)
+        shutil.rmtree(DOWNLOAD_DIRECTORY)
 
 setuptools.setup(name="AutoSklearn",
                  description="Code to participate in the AutoML 2015 challenge.",
@@ -11,8 +101,9 @@ setuptools.setup(name="AutoSklearn",
                  # When we have a package, uncomment this
                  ext_modules=[Extension("autosklearn.data.cdata_manager", ["autosklearn/data/cdata_manager.c"]),
                               Extension("autosklearn.data.chelper_functions", ["autosklearn/data/chelper_functions.c"])],
-                 packages=setuptools.find_packages(),
+                 packages=setuptools.find_packages(exclude=['test']),
                  install_requires=["numpy",
+                                   "psutil",
                                    "pyyaml",
                                    "scipy",
                                    "scikit-learn==0.15.2",
@@ -23,14 +114,14 @@ setuptools.setup(name="AutoSklearn",
                                    "pymetalearn",
                                    "cma"],
                  test_suite="nose.collector",
-                 package_data={'': ['*.txt', '*.md', 'metadata']},
+                 cmdclass={'install': Download},
+                 scripts=["scripts/autosklearn"],
+                 include_package_data=True,
                  author="Matthias Feurer",
                  author_email="feurerm@informatik.uni-freiburg.de",
                  license="BSD",
                  platforms=['Linux'],
                  classifiers=[],
                  url='www.automl.org')
-
-
 
 
