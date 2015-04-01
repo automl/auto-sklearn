696ab9f0b9d43168429e51f9214fd5f68f338ea7
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2015-04-01 14:41:31 +0200

    Raise ValueError if Select Percentile mistakenly removes all features

--- ParamSklearn/components/preprocessing/select_percentile.py
+++ ParamSklearn/components/preprocessing/select_percentile.py
@@ -13,4 +13,7 @@ class SelectPercentileBase(object):
     def transform(self, X):
         if self.preprocessor is None:
             raise NotImplementedError()
-        return self.preprocessor.transform(X)
+        Xt = self.preprocessor.transform(X)
+        if Xt.shape[1] == 0:
+            raise ValueError("%s removed all features." % self.__class__.__name__)
+        return Xt
