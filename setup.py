4c513146e3cb8743fabad23c886220676b7302d6
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-08-28 12:41:15 +0600

    EDIT: setup

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-import os
-import shutil
 from distutils.extension import Extension
 import subprocess
 
@@ -9,21 +7,7 @@ from setuptools.command.install import install
 from pip.req import parse_requirements
 
 from scripts import download_binaries
-
-install_reqs = parse_requirements('requirements.txt')
-
 from autosklearn.conf import SCRIPT_COMPILE_C_UTILS
-
-
-SMAC_DOWNLOAD_LOCATION = 'http://aad.informatik.uni-freiburg.de/~feurerm/'
-SMAC_TAR_NAME = 'smac-v2.08.01-development-1.tar.gz'
-# METADATA_LOCATION = "http://aad.informatik.uni-freiburg.de/~feurerm/"
-# METADATA_TAR_NAME = "metadata_automl1_000.tar.gz"
-RUNSOLVER_LOCATION = 'http://www.cril.univ-artois.fr/~roussel/runsolver/'
-RUNSOLVER_TAR_NAME = 'runsolver-3.3.4.tar.bz2'
-DOWNLOAD_DIRECTORY = os.path.join(os.path.dirname(__file__), '.downloads')
-BINARIES_DIRECTORY = 'autosklearn/binaries'
-METADATA_DIRECTORY = 'autosklearn/metalearning/files'
 
 
 class Download(install):
@@ -40,11 +24,12 @@ class Download(install):
 setuptools.setup(
     name='AutoSklearn',
     description='Code to participate in the AutoML 2015 challenge.',
-    version='0.0.1dev',
+    version='0.0.2dev',
     ext_modules=[Extension('autosklearn.data.competition_c_functions',
                            ['autosklearn/data/competition_c_functions.c'])],
     packages=setuptools.find_packages(exclude=['test']),
-    install_requires=[str(ir.req) for ir in install_reqs],
+    install_requires=[str(ir.req) for ir in
+                      parse_requirements('requirements.txt')],
     test_suite='nose.collector',
     cmdclass={'install': Download},
     scripts=['scripts/autosklearn'],
