57cdca8a513336d30be6cf1daf759cca259abc3a
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-08-28 14:12:21 +0600

    EDIT: setup

--- scripts/compile_c_utils.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# WARNING: hindi code
-
-BASE_FOLDER=$(pwd)
-C_UTILS_FOLDER=$BASE_FOLDER/autosklearn/c_utils
-cd $C_UTILS_FOLDER
-
-
-python setup.py build_ext --build-lib $BASE_FOLDER --build-temp /tmp/c_utils
--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import absolute_import
-from distutils.extension import Extension
 import subprocess
 
 from pip.download import PipSession
-
 import setuptools
 from setuptools.command.install import install
-
 from pip.req import parse_requirements
 
-from conf import SCRIPT_COMPILE_C_UTILS, SCRIPT_INSTALL_REQS
+from conf import SCRIPT_INSTALL_REQS
 from scripts import download_binaries
+
 
 class Download(install):
 
@@ -21,15 +19,19 @@ class Download(install):
         download_binaries()
         subprocess.call(['bash', SCRIPT_INSTALL_REQS])
 
-        subprocess.call(['bash', SCRIPT_COMPILE_C_UTILS])
+        from Cython.Build import cythonize
+
+        ext = cythonize('autosklearn/c_utils/competition_c_functions.pyx')[0]
+        ext.name = 'c_utils'
+        ext.version = '0.1'
+        self.ext_modules = [ext]
         install.do_egg_install(self)
+
 
 setuptools.setup(
     name='AutoSklearn',
     description='Code to participate in the AutoML 2015 challenge.',
     version='0.0.2dev',
-    ext_modules=[Extension('autosklearn.c_utils.competition_c_functions',
-                           ['autosklearn/c_utils/competition_c_functions.c'])],
     packages=setuptools.find_packages(exclude=['test']),
     install_requires=[str(ir.req) for ir in
                       parse_requirements('requirements.txt',
@@ -44,6 +46,3 @@ setuptools.setup(
     platforms=['Linux'],
     classifiers=[],
     url='www.automl.org')
-
-# subprocess.call(['bash %s' % SCRIPT_COMPILE_C_UTILS])
-# subprocess.call(['bash', SCRIPT_COMPILE_C_UTILS])
