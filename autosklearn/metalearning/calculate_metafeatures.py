bd2cb15bd29d82c82b1729b0f0bbd44d66a526cd
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2015-09-25 15:44:54 +0200

    Allow metafeature calculation from arff files

--- autosklearn/metalearning/calculate_metafeatures.py
+++ autosklearn/metalearning/calculate_metafeatures.py
@@ -4,19 +4,16 @@ import os
 
 import numpy as np
 
-from autosklearn.data.competition_data_manager import CompetitionDataManager
+import autosklearn.data.data_manager_factory as data_manager_factory
 
 import autosklearn.metalearning.metafeatures.metafeatures
 import autosklearn.metalearning.metafeatures.metafeature
 
 
-def calculate_metafeatures(dataset_name, dataset_dir, output_dir,
-                           memory_limit=3000):
+def calculate_metafeatures(D, output_dir, memory_limit=3000):
     resource.setrlimit(resource.RLIMIT_AS, (memory_limit * 1048576L, -1L))
-    mf_filename = os.path.join(output_dir, "%s.arff" % dataset_name)
+    mf_filename = os.path.join(output_dir, "%s.arff" % D.basename)
 
-    D = CompetitionDataManager(dataset_name, dataset_dir, verbose=False,
-                    encode_labels=False)
     X = D.data["X_train"]
     y = D.data["Y_train"]
 
@@ -28,9 +25,9 @@ def calculate_metafeatures(dataset_name, dataset_dir, output_dir,
         categorical = [True if c.lower() == "categorical" else False
                        for c in D.feat_type]
 
-    _metafeatures_labels = autosklearn.metalearning.metafeatures.metafeatures.\
+    _metafeatures_labels = autosklearn.metalearning.metafeatures.metafeatures. \
         calculate_all_metafeatures_with_labels(
-        X, y, categorical, dataset_name)
+        X, y, categorical, D.basename)
 
     # Dump the metafeatures for safety in case that there is a crash later
     # http://stackoverflow.com/questions/14906962/python-double-free-error-for-huge-datasets
@@ -42,19 +39,19 @@ def calculate_metafeatures(dataset_name, dataset_dir, output_dir,
     categorical = [False] * X.shape[1]
 
     try:
-        _metafeatures_encoded_labels = autosklearn.metalearning.metafeatures.\
+        _metafeatures_encoded_labels = autosklearn.metalearning.metafeatures. \
             metafeatures.calculate_all_metafeatures_encoded_labels(
-            X, y, categorical, dataset_name)
+            X, y, categorical, D.basename)
     except MemoryError as e:
         # During the conversion of the dataset (rescaling, etc...), it can
         # happen that we run out of memory.
         _metafeatures_encoded_labels = \
             autosklearn.metalearning.metafeatures.metafeature.DatasetMetafeatures(
-                dataset_name, dict())
+                D.basename, dict())
         for metafeature_name in \
                 autosklearn.metalearning.metafeatures.metafeatures.npy_metafeatures:
             type_ = "HELPERFUNCTION" if metafeature_name not in \
-                autosklearn.metalearning.metafeatures.metafeatures.metafeatures.functions \
+                                        autosklearn.metalearning.metafeatures.metafeatures.metafeatures.functions \
                 else "METAFEATURE"
             _metafeatures_encoded_labels.metafeature_values[metafeature_name] = \
                 autosklearn.metalearning.metafeatures.metafeature.MetaFeatureValue(
@@ -72,11 +69,10 @@ def calculate_metafeatures(dataset_name, dataset_dir, output_dir,
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("dataset_name")
-    parser.add_argument("dataset_dir")
-    parser.add_argument("output_dir")
+    parser.add_argument("--output-dir", type=str, required=True)
     parser.add_argument("--memory_limit", type=int, default=3000)
+    parser = data_manager_factory.populate_argparse_with_data_options(parser)
     args = parser.parse_args()
 
-    mf = calculate_metafeatures(args.dataset_name, args.dataset_dir,
-                                args.output_dir, args.memory_limit)
+    D = data_manager_factory.get_data_manager(args, encode_labels=False)
+    mf = calculate_metafeatures(D, args.output_dir, args.memory_limit)
