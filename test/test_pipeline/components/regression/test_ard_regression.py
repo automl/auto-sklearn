cc5d1fb5630f7422a147a42325c8c9a9459ce17f
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-01-07 10:26:00 +0100

    ADD ARD Regression

---
+++ test/test_pipeline/components/regression/test_ard_regression.py
@@ -0,0 +1,17 @@
+import unittest
+
+from autosklearn.pipeline.components.regression.ard_regression import \
+    ARDRegression
+from autosklearn.pipeline.util import _test_regressor
+
+import sklearn.metrics
+
+
+class ARDRegressionComponentTest(unittest.TestCase):
+    def test_default_configuration(self):
+        for i in range(10):
+            predictions, targets = \
+                _test_regressor(ARDRegression, dataset='boston')
+            self.assertAlmostEqual(0.70316694175513961,
+                                   sklearn.metrics.r2_score(targets,
+                                                            predictions))
