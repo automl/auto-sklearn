42f8c1c7afcc97d797366786c876fac610ca5147
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-01-09 12:20:05 +0100

    ADD extra trees as regression feature preprocessor

--- autosklearn/pipeline/components/feature_preprocessing/extra_trees_preproc_for_regression.py
+++ autosklearn/pipeline/components/feature_preprocessing/extra_trees_preproc_for_regression.py
@@ -10,20 +10,19 @@ from autosklearn.pipeline.components.base import \
 from autosklearn.pipeline.constants import *
 
 
-class ExtraTreesPreprocessorClassification(AutoSklearnPreprocessingAlgorithm):
+class ExtraTreesPreprocessorRegression(AutoSklearnPreprocessingAlgorithm):
 
     def __init__(self, n_estimators, criterion, min_samples_leaf,
                  min_samples_split, max_features,
                  max_leaf_nodes_or_max_depth="max_depth",
                  bootstrap=False, max_leaf_nodes=None, max_depth="None",
                  min_weight_fraction_leaf=0.0,
-                 oob_score=False, n_jobs=1, random_state=None, verbose=0,
-                 class_weight=None):
+                 oob_score=False, n_jobs=1, random_state=None, verbose=0):
 
         self.n_estimators = int(n_estimators)
         self.estimator_increment = 10
-        if criterion not in ("gini", "entropy"):
-            raise ValueError("'criterion' is not in ('gini', 'entropy'): "
+        if criterion not in ("mse", ):
+            raise ValueError("'criterion' is not in ('mse', ): "
                              "%s" % criterion)
         self.criterion = criterion
 
@@ -58,26 +57,25 @@ class ExtraTreesPreprocessorClassification(AutoSklearnPreprocessingAlgorithm):
         self.n_jobs = int(n_jobs)
         self.random_state = random_state
         self.verbose = int(verbose)
-        self.class_weight = class_weight
         self.preprocessor = None
 
-    def fit(self, X, Y, sample_weight=None):
-        from sklearn.ensemble import ExtraTreesClassifier
+    def fit(self, X, Y):
+        from sklearn.ensemble import ExtraTreesRegressor
 
         num_features = X.shape[1]
         max_features = int(
             float(self.max_features) * (np.log(num_features) + 1))
         # Use at most half of the features
         max_features = max(1, min(int(X.shape[1] / 2), max_features))
-        self.preprocessor = ExtraTreesClassifier(
+        self.preprocessor = ExtraTreesRegressor(
             n_estimators=0, criterion=self.criterion,
             max_depth=self.max_depth, min_samples_split=self.min_samples_split,
             min_samples_leaf=self.min_samples_leaf, bootstrap=self.bootstrap,
             max_features=max_features, max_leaf_nodes=self.max_leaf_nodes,
             oob_score=self.oob_score, n_jobs=self.n_jobs, verbose=self.verbose,
-            random_state=self.random_state, class_weight=self.class_weight
-        )
-        self.preprocessor.fit(X, Y, sample_weight=sample_weight)
+            random_state=self.random_state)
+        self.preprocessor.fit(X, Y)
+
         return self
 
     def transform(self, X):
@@ -87,18 +85,18 @@ class ExtraTreesPreprocessorClassification(AutoSklearnPreprocessingAlgorithm):
 
     @staticmethod
     def get_properties(dataset_properties=None):
-        return {'shortname': 'ETC',
-                'name': 'Extra Trees Classifier Preprocessing',
+        return {'shortname': 'ETR',
+                'name': 'Extra Trees Regressor Preprocessing',
                 'handles_missing_values': False,
                 'handles_nominal_values': False,
                 'handles_numerical_features': True,
                 'prefers_data_scaled': False,
                 # TODO find out if this is good because of sparcity...
                 'prefers_data_normalized': False,
-                'handles_regression': False,
-                'handles_classification': True,
-                'handles_multiclass': True,
-                'handles_multilabel': True,
+                'handles_regression': True,
+                'handles_classification': False,
+                'handles_multiclass': False,
+                'handles_multilabel': False,
                 'is_deterministic': True,
                 'handles_sparse': True,
                 'input': (DENSE, SPARSE, UNSIGNED_DATA),
@@ -112,8 +110,7 @@ class ExtraTreesPreprocessorClassification(AutoSklearnPreprocessingAlgorithm):
         cs = ConfigurationSpace()
 
         n_estimators = cs.add_hyperparameter(Constant("n_estimators", 100))
-        criterion = cs.add_hyperparameter(CategoricalHyperparameter(
-            "criterion", ["gini", "entropy"], default="gini"))
+        criterion = cs.add_hyperparameter(Constant("criterion", "mse"))
         max_features = cs.add_hyperparameter(UniformFloatHyperparameter(
             "max_features", 0.5, 5, default=1))
 
