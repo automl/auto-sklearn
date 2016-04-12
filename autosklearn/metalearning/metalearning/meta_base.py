e0a7bec206d14d8440a0b4df53bde7e4d867c491
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-04-12 21:40:17 +0200

    ADD predict across datasets

--- autosklearn/metalearning/metalearning/meta_base.py
+++ autosklearn/metalearning/metalearning/meta_base.py
@@ -7,9 +7,6 @@ from ..metafeatures.metafeature import DatasetMetafeatures
 from ConfigSpace.configuration_space import Configuration
 
 
-
-
-
 class Run(object):
     def __init__(self, configuration, result, runtime):
         self.configuration = configuration
@@ -43,6 +40,9 @@ class MetaBase(object):
         self.metafeatures = aslib_reader.metafeatures
         self.algorithm_runs = aslib_reader.algorithm_runs
         self.configurations = aslib_reader.configurations
+
+        self.logger.critical('%s %s %s', self.metafeatures,
+                             self.algorithm_runs, self.configurations)
 
         configurations = dict()
         for algorithm_id in self.configurations:
@@ -79,13 +79,26 @@ class MetaBase(object):
             performance_measure = list(self.algorithm_runs.keys())[0]
         return self.algorithm_runs[performance_measure]
 
-    def get_metafeatures(self, dataset_name):
-        dataset_metafeatures = self.metafeatures.loc[dataset_name]
-        return dataset_metafeatures
+    def get_metafeatures(self, dataset_name=None, features=None):
+        if features is not None:
+            metafeatures = self._get_metafeatures(features)
+        else:
+            metafeatures = self.metafeatures
+        if dataset_name is not None:
+            return metafeatures.loc[dataset_name]
+        else:
+            return metafeatures
 
-    def get_all_metafeatures(self):
-        """Create a pandas DataFrame for the metadata of all datasets."""
-        return self.metafeatures
+    def _get_metafeatures(self, features):
+        """This is inside an extra function for testing purpose"""
+        # Load the task
+
+        self.logger.info("Going to use the following metafeature subset: %s",
+                         features)
+        all_metafeatures = self.metafeatures
+        all_metafeatures = all_metafeatures.loc[:, features]
+
+        return all_metafeatures
 
     def get_configuration_from_algorithm_index(self, idx):
         return self.configurations[idx]
@@ -100,3 +113,6 @@ class MetaBase(object):
                 return idx
 
         raise ValueError(configuration)
+
+    def get_all_dataset_names(self):
+        return list(self.metafeatures.index)
