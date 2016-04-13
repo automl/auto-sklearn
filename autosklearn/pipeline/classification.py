685e4111f801b2a2e200efde5ca88f22bcdb54f9
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-04-13 09:19:05 +0200

    FIX don't use xgradient boosting with nonlinear feature preprocessing

--- autosklearn/pipeline/classification.py
+++ autosklearn/pipeline/classification.py
@@ -202,7 +202,7 @@ class SimpleClassificationPipeline(ClassifierMixin, BasePipeline):
         classifiers_ = ["adaboost", "decision_tree", "extra_trees",
                         "gradient_boosting", "k_nearest_neighbors",
                         "libsvm_svc", "random_forest", "gaussian_nb",
-                        "decision_tree"]
+                        "decision_tree", "xgradient_boosting"]
         feature_learning = ["kitchen_sinks", "nystroem_sampler"]
 
         for c, f in product(classifiers_, feature_learning):
