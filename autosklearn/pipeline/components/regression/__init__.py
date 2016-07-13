92a893517e35f602b5f76825c217d52cefae18fc
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-07-13 17:56:59 +0200

    MAINT dedup code in choice objects

--- autosklearn/pipeline/components/regression/__init__.py
+++ autosklearn/pipeline/components/regression/__init__.py
@@ -98,51 +98,11 @@ class RegressorChoice(AutoSklearnChoice):
                                               default=default)
         cs.add_hyperparameter(estimator)
         for estimator_name in available_estimators.keys():
-
-            # We have to retrieve the configuration space every time because
-            # we change the objects it returns. If we reused it, we could not
-            # retrieve the conditions further down
-            # TODO implement copy for hyperparameters and forbidden and
-            # conditions!
-
-            estimator_configuration_space = available_estimators[
-                estimator_name]. \
+            estimator_configuration_space = available_estimators[estimator_name].\
                 get_hyperparameter_search_space(dataset_properties)
-            for parameter in estimator_configuration_space.get_hyperparameters():
-                new_parameter = copy.deepcopy(parameter)
-                new_parameter.name = "%s:%s" % (
-                    estimator_name, new_parameter.name)
-                cs.add_hyperparameter(new_parameter)
-                # We must only add a condition if the hyperparameter is not
-                # conditional on something else
-                if len(estimator_configuration_space.
-                        get_parents_of(parameter)) == 0:
-                    condition = EqualsCondition(new_parameter, estimator,
-                                                estimator_name)
-                    cs.add_condition(condition)
-
-            for condition in available_estimators[estimator_name]. \
-                    get_hyperparameter_search_space(
-                    dataset_properties).get_conditions():
-                dlcs = condition.get_descendant_literal_conditions()
-                for dlc in dlcs:
-                    if not dlc.child.name.startswith(estimator_name):
-                        dlc.child.name = "%s:%s" % (
-                            estimator_name, dlc.child.name)
-                    if not dlc.parent.name.startswith(estimator_name):
-                        dlc.parent.name = "%s:%s" % (
-                            estimator_name, dlc.parent.name)
-                cs.add_condition(condition)
-
-            for forbidden_clause in available_estimators[estimator_name]. \
-                    get_hyperparameter_search_space(
-                    dataset_properties).forbidden_clauses:
-                dlcs = forbidden_clause.get_descendant_literal_clauses()
-                for dlc in dlcs:
-                    if not dlc.hyperparameter.name.startswith(estimator_name):
-                        dlc.hyperparameter.name = "%s:%s" % (estimator_name,
-                                                             dlc.hyperparameter.name)
-                cs.add_forbidden_clause(forbidden_clause)
+            parent_hyperparameter = {'parent': estimator, 'value': estimator_name}
+            cs.add_configuration_space(estimator_name, estimator_configuration_space,
+                                       parent_hyperparameter=parent_hyperparameter)
 
         self.configuration_space_ = cs
         self.dataset_properties_ = dataset_properties
