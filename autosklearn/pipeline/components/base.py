a18252a71dd53cc7d24512107e6d0dcfcff563c2
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-12-15 15:31:08 +0100

    MAINT remove print statement from main code

--- autosklearn/pipeline/components/base.py
+++ autosklearn/pipeline/components/base.py
@@ -59,7 +59,6 @@ class ThirdPartyComponents(object):
                 raise ValueError('Property %s not specified for algorithm %s')
 
         self.components[name] = classifier
-        print(name, classifier)
 
 
 class AutoSklearnClassificationAlgorithm(object):
