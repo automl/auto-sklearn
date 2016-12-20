5da30efc6cbbc58db60ba29643c56448b5a79e77
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-12-20 22:36:58 +0100

    FIX fix unit test by fixing import paths

--- test/test_pipeline/components/test_base.py
+++ test/test_pipeline/components/test_base.py
@@ -1,13 +1,19 @@
+import os
+import sys
 import unittest
 
 from autosklearn.pipeline.components.base import find_components, \
     AutoSklearnClassificationAlgorithm
 
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(this_dir)
+
 
 class TestBase(unittest.TestCase):
 
     def test_find_components(self):
-        c = find_components('dummy_components', 'dummy_components',
+        c = find_components('dummy_components',
+                            os.path.join(this_dir, 'dummy_components'),
                             AutoSklearnClassificationAlgorithm)
         self.assertEqual(len(c), 2)
         self.assertEqual(c['dummy_component_1'].__name__, 'DummyComponent1')
