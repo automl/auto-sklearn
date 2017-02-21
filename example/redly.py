ee7054e4508db7ce655881e05cccd04e9c4444b2
Diego Kobylkin <Motorrat@users.noreply.github.com>
2017-02-21 16:01:13 +0100

    Create redly.py

---
+++ example/redly.py
@@ -0,0 +1,2 @@
+# A placeholder for a fully automated command-line wrapper for auto-sklearn binary classification
+# It takes the dataset file as the only parameter and predicts in a 'yes or now' kind of situation
