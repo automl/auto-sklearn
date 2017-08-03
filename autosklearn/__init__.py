7b400b35dd70b2484ba9c56bdc5b3d961ba74710
Swier <swierh@users.noreply.github.com>
2017-08-03 10:52:57 +0200

    set sklearn dependency to GTEQ 0.18.1
    from equal to 0.18.1, as sklearn is now in version 0.18.2

--- autosklearn/__init__.py
+++ autosklearn/__init__.py
@@ -5,7 +5,7 @@ from autosklearn.__version__ import __version__
 
 __MANDATORY_PACKAGES__ = '''
 numpy>=1.9
-scikit-learn==0.18.1
+scikit-learn>=0.18.1
 smac==0.5.0
 lockfile>=0.10
 ConfigSpace>=0.3.3,<0.4
