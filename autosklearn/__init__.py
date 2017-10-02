0001c0cc327e08d4bcf290ca312e2f9c73b22d25
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2017-10-02 15:19:23 +0200

    MAINT use bugfixed version of pyrfr

--- autosklearn/__init__.py
+++ autosklearn/__init__.py
@@ -6,8 +6,9 @@ from autosklearn.__version__ import __version__
 __MANDATORY_PACKAGES__ = '''
 numpy>=1.9
 scikit-learn>=0.18.1,<0.19
-smac==0.6.0
 lockfile>=0.10
+smac>=0.6.0,<0.7
+pyrfr>=0.6.1,<0.7
 ConfigSpace>=0.3.3,<0.4
 pyrfr>=0.6.0,<0.7
 '''
