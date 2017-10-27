5bf6a3a617fdfeda97ff364e9e38da90c60621ec
Jesper van Engelen <contact@jepps.nl>
2017-10-27 13:26:00 +0200

    Update default result values for forest tests

--- test/test_pipeline/components/classification/test_extra_trees.py
+++ test/test_pipeline/components/classification/test_extra_trees.py
@@ -15,10 +15,10 @@ class ExtraTreesComponentTest(BaseClassificationComponentTest):
     res["default_iris_iterative"] = 0.93999999999999995
     res["default_iris_proba"] = 0.1086791056721286
     res["default_iris_sparse"] = 0.71999999999999997
-    res["default_digits"] = 0.8986035215543412
-    res["default_digits_iterative"] = 0.81785063752276865
-    res["default_digits_binary"] = 0.99392835458409234
-    res["default_digits_multilabel"] = 0.99746415200960636
+    res["default_digits"] = 0.91499696417729204
+    res["default_digits_iterative"] = 0.85428051001821492
+    res["default_digits_binary"] = 0.99453551912568305
+    res["default_digits_multilabel"] = 0.99838668373879624
     res["default_digits_multilabel_proba"] = 0.99628313054140716
 
     sk_mod = sklearn.ensemble.ExtraTreesClassifier
