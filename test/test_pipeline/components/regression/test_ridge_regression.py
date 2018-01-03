428e0da1e843889bf391c99bf4ed2c167d7c82aa
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2018-01-03 16:16:29 +0100

    TSET reduce places for ridge regression test

--- test/test_pipeline/components/regression/test_ridge_regression.py
+++ test/test_pipeline/components/regression/test_ridge_regression.py
@@ -15,6 +15,7 @@ class RidgeComponentTest(BaseRegressionComponentTest):
     res["default_boston_sparse"] = 0.1086535440527433
     res["default_boston_iterative_sparse"] = None
     res["default_diabetes"] = 0.32614416980439365
+    res["default_diabetes_places"] = 5
     res["default_diabetes_iterative"] = None
     res["default_diabetes_sparse"] = 0.1298840412368325
     res["default_diabetes_iterative_sparse"] = None
