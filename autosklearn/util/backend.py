d346fd4b2b9589ab4c4ea54bfb0d068755b938be
Manuel Streuhofer <manuel@streuhofer.net>
2018-07-29 14:27:35 +0200

    follow quality review coding standards

--- autosklearn/util/backend.py
+++ autosklearn/util/backend.py
@@ -71,11 +71,17 @@ class BackendContext(object):
 
         self.__temporary_directory = temporary_directory \
             if temporary_directory \
-            else os.path.join(tempfile.gettempdir(), 'autosklearn_tmp_%d_%d' % (pid, random_number))
+            else os.path.join(
+                tempfile.gettempdir(),
+                'autosklearn_tmp_%d_%d' % (pid, random_number)
+            )
 
         self.__output_directory = output_directory \
             if output_directory \
-            else os.path.join(tempfile.gettempdir(), 'autosklearn_output_%d_%d' % (pid, random_number))
+            else os.path.join(
+                tempfile.gettempdir(),
+                'autosklearn_output_%d_%d' % (pid, random_number)
+            )
 
     def create_directories(self):
         if self.shared_mode:
