b73b9d0823e4a83607347709dfa7c32758c0491e
Jinu <ahnj@cs.uni-freiburg.de>
2018-09-23 19:40:06 +0200

    Fix minor details

--- test/test_util/test_logging.py
+++ test/test_util/test_logging.py
@@ -12,7 +12,7 @@ class LoggingTest(unittest.TestCase):
         # according to the given dictionary, and uses the default
         # logging.yaml file if logging_config is not specified.
         with open(os.path.join('example_config.yaml'), 'r') as fh:
-            example_config = yaml.load(fh)
+            example_config = yaml.safe_load(fh)
 
         # Configure logger with example_config.yaml.
         logging_.setup_logger(logging_config=example_config)
