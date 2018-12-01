0c79a0aac426597767c77cd6711d0a1bbe8f1404
diogo-bastos <dnmendesbastos@gmail.com>
2018-12-01 19:19:23 +0000

    fix: removed trailing whitespaces

--- autosklearn/pipeline/components/classification/decision_tree.py
+++ autosklearn/pipeline/components/classification/decision_tree.py
@@ -39,7 +39,7 @@ class DecisionTree(AutoSklearnClassificationAlgorithm):
         else:
             num_features = X.shape[1]
             self.max_depth_factor = int(self.max_depth_factor)
-            max_depth_factor = max(1, 
+            max_depth_factor = max(1,
 				int(np.round(self.max_depth_factor * num_features, 0)))
         self.min_samples_split = int(self.min_samples_split)
         self.min_samples_leaf = int(self.min_samples_leaf)
