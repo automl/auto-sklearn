def630baed43fe759f3e64969b4c25ab64578e03
Jinu <ahnj@cs.uni-freiburg.de>
2018-12-14 11:27:38 +0100

    Save current progress

--- scripts/2015_nips_paper/get_tasks.py
+++ scripts/2015_nips_paper/get_tasks.py
@@ -1,5 +1,4 @@
 # get tasks from openml
-
 import openml
 import pandas as pd
 from pprint import pprint
@@ -15,10 +14,10 @@ def read_csv(file):
     return dataset_ids
 
 def get_tasks_ids(dataset_ids):
-    # returns a list containing tasj_ids
+    # return task ids of corresponding datset ids.
     task_ids = []
 
-    # Some datasets are deactivated. We remove them from the list.
+    # Some datasets are deactivated. We remove them from the list first.
     deactivated_list = openml.datasets.list_datasets(status='deactivated')
     deactivated_list = list(deactivated_list.keys())
     for idx in deactivated_list:
@@ -31,16 +30,16 @@ def get_tasks_ids(dataset_ids):
     tasks = pd.DataFrame.from_dict(tasks, orient="index")
 
     for did in dataset_ids:
-        task = tasks.query('did == {}'.format(did))
+        task = tasks.query('did == {}'.format(did))  # find all tasks with given dataset id.
         task = list(task.tid)
-        task_ids.append(task[0])  # append first task id which has given dataset id.
+        task_ids.append(task[0])  # append the first task id found.
 
     return task_ids
 
 def main():
     datasets = 'resources/datasets.csv'
     dataset_ids = read_csv(datasets)
-    task_ids = get_tasks_ids(dataset_ids)
+    task_ids = sorted(get_tasks_ids(dataset_ids))
     string_to_print = ''
     for tid in task_ids:
         string_to_print += str(tid) + ' '
@@ -49,9 +48,6 @@ def main():
 
 
 if __name__=="__main__":
-    tasks = openml.tasks.list_tasks(task_type_id=1)
-    # For each tasks, if tasks.did in [dataset_ids]: remember the its task id.
-
     main()
 
 
