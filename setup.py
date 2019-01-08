185170ec570c29670f9384c0c0118ab152e6e232
Jirka Borovec <Borda@users.noreply.github.com>
2019-01-08 16:06:16 +0100

    fix installation from pip (#611)
    * refactor using numpy and cython
    
    * use requirements @setup
    
    * remove compile flags & add setup tags

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 # -*- encoding: utf-8 -*-
-import setuptools
-from setuptools.extension import Extension
-import numpy as np
-from Cython.Build import cythonize
 import os
 import sys
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
 
 
 # Check if Auto-sklearn *could* run on the given system
@@ -23,51 +22,71 @@ if sys.version_info < (3, 5):
     )
 
 
-extensions = cythonize(
-    [Extension('autosklearn.data.competition_c_functions',
-               sources=['autosklearn/data/competition_c_functions.pyx'],
-               language='c',
-               include_dirs=[np.get_include()])
-     ])
+class BuildExt(build_ext):
+    """ build_ext command for use when numpy headers are needed.
+    SEE tutorial: https://stackoverflow.com/questions/2379898
+    SEE fix: https://stackoverflow.com/questions/19919905
+    """
 
-requirements = [
-    "setuptools",
-    "nose",
-    "Cython",
-    # Numpy version of higher than 1.14.5 causes libgcc_s.so.1 error.
-    "numpy>=1.9.0,<=1.14.5",
-    "scipy>=0.14.1",
-    "scikit-learn>=0.19,<0.20",
-    "lockfile",
-    "joblib",
-    "psutil",
-    "pyyaml",
-    "liac-arff",
-    "pandas",
-    "ConfigSpace>=0.4.0,<0.5",
-    "pynisher>=0.4.2",
-    "pyrfr>=0.6.1,<0.8",
-    "smac>=0.8,<0.9",
-    "xgboost>=0.80",
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        # __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+extensions = [
+    Extension('autosklearn.data.competition_c_functions',
+              sources=['autosklearn/data/competition_c_functions.pyx'],
+              language='c')
 ]
+
+
+def _parse_requirements(file_path):
+    with open(file_path) as fp:
+        reqs = [r.rstrip() for r in fp.readlines() if not r.startswith('#')]
+        return reqs
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+setup_reqs = ['Cython', 'numpy']
+with open(os.path.join(HERE, 'requirements.txt')) as fp:
+    install_reqs = [r.rstrip() for r in fp.readlines()
+                    if not r.startswith('#')]
 
 with open("autosklearn/__version__.py") as fh:
     version = fh.readlines()[-1].split()[-1].strip("\"'")
 
-setuptools.setup(
+setup(
     name='auto-sklearn',
-    description='Automated machine learning.',
-    version=version,
-    ext_modules=extensions,
-    packages=setuptools.find_packages(exclude=['test']),
-    install_requires=requirements,
-    test_suite='nose.collector',
-    include_package_data=True,
     author='Matthias Feurer',
     author_email='feurerm@informatik.uni-freiburg.de',
+    description='Automated machine learning.',
+    version=version,
+    cmdclass={'build_ext': BuildExt},
+    ext_modules=extensions,
+    packages=find_packages(exclude=['test', 'scripts', 'examples']),
+    setup_requires=setup_reqs,
+    install_requires=install_reqs,
+    test_suite='nose.collector',
+    include_package_data=True,
     license='BSD',
     platforms=['Linux'],
-    classifiers=[],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     python_requires='>=3.5.*',
     url='https://automl.github.io/auto-sklearn',
 )
