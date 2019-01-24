198a43ee1816cefe4d07ea92af3451438057b77f
jinu <ahn1340@hotmail.com>
2019-01-24 20:14:49 +0100

    Fix travis error

--- autosklearn/smbo.py
+++ autosklearn/smbo.py
@@ -18,7 +18,7 @@ from smac.optimizer import pSMAC
 
 import autosklearn.metalearning
 from autosklearn.constants import MULTILABEL_CLASSIFICATION, \
-    BINARY_CLASSIFICATION, TASK_TYPES_TO_STRING
+    BINARY_CLASSIFICATION, TASK_TYPES_TO_STRING, CLASSIFICATION_TASKS
 from autosklearn.metalearning.mismbo import suggest_via_metalearning
 from autosklearn.data.abstract_data_manager import AbstractDataManager
 from autosklearn.data.competition_data_manager import CompetitionDataManager
--- test/test_metalearning/test_metalearning.py
+++ test/test_metalearning/test_metalearning.py
@@ -1,8 +1,6 @@
 # -*- encoding: utf-8 -*-
-import os
 import unittest
 
-import autosklearn.metalearning
 from autosklearn.pipeline.util import get_dataset
 from autosklearn.classification import AutoSklearnClassifier
 
