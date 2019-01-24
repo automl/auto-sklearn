e38e1224a631d15d600d363b998ec8f6229960a0
jinu <ahn1340@hotmail.com>
2019-01-24 18:31:23 +0100

    Modify

--- scripts/2015_nips_paper/score_metalearning.py
+++ scripts/2015_nips_paper/score_metalearning.py
@@ -9,9 +9,31 @@ from autosklearn.metrics import balanced_accuracy
 
 import openml
 openml.config.cache_directory = os.path.join(os.path.expanduser("~"), 'openml') # Home directory. change this later accordingly
-sys.path.append('../')
-from update_metadata_util import load_task  # noqa
 from remove_dataset_from_metadata import remove_dataset
+
+def load_task(task_id):
+    """Function used in score_vanilla and score_metalearning
+    for loading data."""
+    task = openml.tasks.get_task(task_id)
+    X, y = task.get_X_and_y()
+    train_indices, test_indices = task.get_train_test_split_indices()
+    X_train = X[train_indices]
+    y_train = y[train_indices]
+    X_test = X[test_indices]
+    y_test = y[test_indices]
+    dataset = openml.datasets.get_dataset(task.dataset_id)
+    _, _, cat = dataset.get_data(return_categorical_indicator=True,
+                                 target=task.target_name)
+    del _
+    del dataset
+    cat = ['categorical' if c else 'numerical' for c in cat]
+
+    unique = np.unique(y_train)
+    mapping = {unique_value: i for i, unique_value in enumerate(unique)}
+    y_train = np.array([mapping[value] for value in y_train])
+    y_test = np.array([mapping[value] for value in y_test])
+
+    return X_train, y_train, X_test, y_test, cat
 
 def main(working_directory, time_limit, per_run_time_limit, task_id, seed):
     # Load data and other info.
