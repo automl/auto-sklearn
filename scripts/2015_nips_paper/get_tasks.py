5f6de3a6593f516653fc5e7ac9eba58c038de101
jinu <ahn1340@hotmail.com>
2019-01-24 16:48:37 +0100

    Trying figure out which task_ids are missing in the cluster cache

--- scripts/2015_nips_paper/get_tasks.py
+++ scripts/2015_nips_paper/get_tasks.py
@@ -16,25 +16,29 @@ def read_csv(file):
 def get_task_ids(dataset_ids):
     # return task ids of corresponding datset ids.
     # active tasks
+    print(len(dataset_ids))
     tasks_a = openml.tasks.list_tasks(task_type_id=1, status='active')
     tasks_a = pd.DataFrame.from_dict(tasks_a, orient="index")
     # query only those with NaN as evaluation_measures.
-    tasks_a = tasks_a.query("evaluation_measures != evaluation_measures")
+    #tasks_a = tasks_a.query("evaluation_measures != evaluation_measures")
     # query only those with holdout as the resampling startegy.
     tasks_a = tasks_a[(tasks_a.estimation_procedure == "33% Holdout set")]
 
     # deactivated tasks
     tasks_d = openml.tasks.list_tasks(task_type_id=1, status='deactivated')
     tasks_d = pd.DataFrame.from_dict(tasks_d, orient="index")
-    tasks_d = tasks_d.query("evaluation_measures != evaluation_measures")
+    #tasks_d = tasks_d.query("evaluation_measures != evaluation_measures")
     tasks_d = tasks_d[(tasks_d.estimation_procedure == "33% Holdout set")]
 
     task_ids = []
     for did in dataset_ids:
         task_a = list(tasks_a.query("did == {}".format(did)).tid)
         task_d = list(tasks_d.query("did == {}".format(did)).tid)
-        task_ids += task_a + task_d
-    #print(len(task_ids)) there are only 22 tasks!
+        if len(task_a) > 1:  # if there are more than one task, take the first one.
+            task_a = task_a[:1]
+        if len(task_d) > 1:
+            task_d = task_d[:1]
+        task_ids += list(task_a + task_d)
 
     return task_ids  # return list of all task ids.
 
