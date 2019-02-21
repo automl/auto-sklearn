ebed88dbc52dded072b5ad8fbebdd3eeb6eecc10
Jin Woo Ahn <ahnj@informatik.uni-freiburg.de>
2019-02-21 17:18:58 +0100

    Commit so it can pull from origin

--- scripts/2015_nips_paper/job.sh
+++ scripts/2015_nips_paper/job.sh
@@ -4,6 +4,6 @@
 #rm -r test2_folder
 
 python slurm_helper.py commands.txt \
-    --cores 1 --timelimit 30000 --queue ml_cpu-ivy --array_min 1 --array_max 1000 \
-    --output test2_folder --logfiles test2_folder
+    --cores 1 --timelimit 30000 --queue ml_cpu-ivy --array_min 1 --array_max 4000 \
+    --output full_run_log --logfiles full_run_log
 
