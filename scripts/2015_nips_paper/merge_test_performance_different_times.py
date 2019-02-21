076e9f56b4293b8fdcbe7add018456fa2c60d03f
jinu <ahn1340@hotmail.com>
2019-02-21 19:10:27 +0100

    Use pandas series for fill_trajectory

--- scripts/2015_nips_paper/merge_test_performance_different_times.py
+++ scripts/2015_nips_paper/merge_test_performance_different_times.py
@@ -8,75 +8,26 @@ import numpy as np
 
 import plot_util
 import pandas as pd
-#TODO: replace fill_trajectory with pandas! But only after some more experiments
+
 
 def fill_trajectory(performance_list, time_list):
-    # Fill times
+    # Create n series objects.
+    series_list = []
+    for n in range(len(time_list)):
+        series_list.append(pd.Series(data=performance_list[n], index=time_list[n]))
 
-    empty_elements = []
-    for i, p_ in enumerate(performance_list):
-        if len(p_) == 0:
-            empty_elements.append(i)
-    for i in empty_elements[-1::-1]:
-        del performance_list[i]
-        del time_list[i]
+    # Concatenate to one Series with NaN vales.
+    series = pd.concat(series_list, axis=1)
 
-    len_exp = list([len(i) for i in performance_list])
-    num_exp = len(performance_list)
+    # Fill missing performance values (NaNs) with last non-NaN value.
+    series = series.fillna(method='ffill')
 
-    time_idx = list([1 for i in range(num_exp)])
-    perf_idx = list([0 for i in range(num_exp)])
-    exp_time = list([0, ])
-    perf_list = [list([performance_list[i][0], ]) for i in range(num_exp)]
+    # Remove all but first time steps with the same value (sometimes the
+    # incumbent does not change over time).
+    series = series.drop_duplicates(keep='first')
 
-    while True:
-        # Find out exp with lowest time
-        t_list = [sys.maxsize if time_idx[i] == -1 else time_list[i][time_idx[i]] for i in range(num_exp)]
-        low_idx = np.argmin(t_list)
-        if t_list[low_idx] == sys.maxsize:
-            break
-        del t_list
-
-        # Append time to the list for this exp
-        exp_time.append(time_list[low_idx][time_idx[low_idx]])
-
-        # Check whether there is any other exp with the same timestep
-        low_time = time_list[low_idx][time_idx[low_idx]]
-        for idx in range(num_exp):
-            if time_list[idx][time_idx[idx]] == low_time:
-                time_idx[idx] += 1
-                perf_idx[idx] += 1
-        del low_time
-
-        # Append performance
-        for i in range(num_exp):
-            perf_list[i].append(performance_list[i][perf_idx[i]])
-
-        # Check whether we are at the end for any exp
-        time_idx = [time_idx[t] if time_idx[t] < len_exp[t] else -1 for t in range(num_exp)]
-        perf_idx = [perf_idx[t] if perf_idx[t] < len_exp[t] else -1 for t in range(num_exp)]
-    # We don't need this anymore
-    del time_idx
-    del perf_idx
-
-    # Now clean data as sometimes the incumbent doesn't change over time
-    last_perf = [i*10 for i in range(num_exp)]  # dummy entry
-    time_ = list()
-    performance = list([list() for i in range(num_exp)])
-    for idx, t in enumerate(exp_time):
-        # print t, idx, last_perf, perf_list[0][idx], perf_list[1][idx]
-        diff = sum([np.abs(last_perf[i] - perf_list[i][idx]) for i in range(num_exp)])
-        if diff != 0 or idx == 0 or idx == len(exp_time) - 1:
-            # always use first and last entry
-            time_.append(t)
-            [performance[i].append(perf_list[i][idx]) for i in range(num_exp)]
-        last_perf = [p[idx] for p in perf_list]
-
-    print("len(performance)", [len(p) for p in performance])
-    # print "Performance", performance
-    print("len(time)", len(time_))
-    # print "Time steps", time_
-    return performance, time_
+    # Returns performance (Numpy array), time steps (list)
+    return series.values, list(series.index)
 
 
 def main():
@@ -87,15 +38,23 @@ def main():
     parser = ArgumentParser(description=description, prog=prog)
 
     # General Options
-    parser.add_argument("--train", action='store_true',
+    #parser.add_argument("--train", action='store_true',
+    #                    help='Read training instead of test data.')
+    #parser.add_argument("--maxvalue", dest="maxvalue", type=float,
+    #                    default=sys.maxsize,
+    #                    help="Replace all values higher than this?")
+    #parser.add_argument("--save", dest="saveTo", type=str,
+    #                    required=True, help="Where to save the csv?")
+    parser.add_argument("--train", action='store_true', default=False,
                         help='Read training instead of test data.')
     parser.add_argument("--maxvalue", dest="maxvalue", type=float,
                         default=sys.maxsize,
                         help="Replace all values higher than this?")
-    parser.add_argument("--save", dest="saveTo", type=str,
-                        required=True, help="Where to save the csv?")
+    parser.add_argument("--save", dest="saveTo", type=str, default="merged.csv",
+                        help="Where to save the csv?")
 
     args, unknown = parser.parse_known_args()
+    unknown = ["score_ensemble1.csv", "score_ensemble2.csv"]
 
     sys.stdout.write("\nFound " + str(len(unknown)) + " arguments\n")
 
@@ -128,7 +87,7 @@ def main():
         csv_data = np.array(csv_data)
         # Replace too high values with args.maxsize
         if len(csv_data) == 0:
-            print ("Empty array in %s" % fl)
+            print("Empty array in %s" % fl)
             continue
         # First column of csv_data: time
         # second column: train performance (vanilla won't have it)
@@ -146,6 +105,8 @@ def main():
         performance_list.append(data)
         time_list.append(time_steps)
 
+    # performance : Numpy array (n_values, n_runs)
+    # time_: list
     performance, time_ = fill_trajectory(performance_list=performance_list, time_list=time_list)
 
     fh = open(args.saveTo, 'w')
@@ -155,7 +116,7 @@ def main():
     writer.writerow(header)
     for r, t in enumerate(time_):
         row = list([t, ])
-        row.extend(["%10.5f" % p[r] for p in performance])
+        row.extend(["%10.5f" % performance[r, i] for i in range(len(file_list))])
         writer.writerow(row)
     fh.close()
 
