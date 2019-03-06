365dc8c5b9c4a361df664b0585c93c5d716dd0b4
jianswang <45787773+jianswang@users.noreply.github.com>
2019-03-06 16:02:56 +0800

    Update automl.py

--- autosklearn/automl.py
+++ autosklearn/automl.py
@@ -756,7 +756,8 @@ class AutoML(BaseEstimator):
 
             param_dict = config.get_dictionary()
             params.append(param_dict)
-            mean_test_score.append(self._metric._optimum - (self._metric._sign * run_value.cost))
+            mean_test_score.append(self._metric._optimum - 
+                                   (self._metric._sign * run_value.cost))
             mean_fit_time.append(run_value.time)
             s = run_value.status
             if s == StatusType.SUCCESS:
