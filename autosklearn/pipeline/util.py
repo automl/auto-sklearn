57b022fa4933d2c9810d1f4126c442ba087d6ccc
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2019-08-15 18:44:27 +0200

    fix bugs?

--- autosklearn/pipeline/util.py
+++ autosklearn/pipeline/util.py
@@ -168,7 +168,8 @@ def _test_classifier_predict_proba(classifier, dataset='iris', sparse=False,
     return predictions, Y_test
 
 
-def _test_preprocessing(Preprocessor, dataset='iris', make_sparse=False, train_size_maximum=150):
+def _test_preprocessing(Preprocessor, dataset='iris', make_sparse=False,
+                        train_size_maximum=150):
     X_train, Y_train, X_test, Y_test = get_dataset(dataset=dataset,
                                                    make_sparse=make_sparse,
                                                    train_size_maximum=train_size_maximum)
