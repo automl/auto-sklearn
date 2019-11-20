888d3eb740fbc6ae83b8a5d18a10abaafc5c24da
Gui Miotto <guilherme.miotto@gmail.com>
2019-11-20 19:11:18 +0100

    added tests for the CategoryShift implementation

--- autosklearn/pipeline/implementations/CategoryShift.py
+++ autosklearn/pipeline/implementations/CategoryShift.py
@@ -1,22 +1,30 @@
 import numpy as np
 from scipy import sparse
+from sklearn.utils import check_array
 
 class CategoryShift:
     """ Add 3 to every category.
     """
     
+    def _convert_and_check_X(self, X, copy):
+        X = check_array(X, accept_sparse='csc', force_all_finite=False, copy=copy,
+            dtype=np.int32)
+        X_data = X.data if sparse.issparse(X) else X
+        if np.nanmin(X_data) < 0:
+            raise ValueError("X needs to contain only non-negative integers.")
+    
+    
     def fit(self, X, y=None):
+        self._convert_and_check_X(X, copy=True)
         return self 
 
     def transform(self, X):
+        self._convert_and_check_X(X, copy=False)
         # Increment everything by three to account for the fact that
         # np.NaN will get an index of two, and coalesced values will get index of
         # one, index of zero is not assigned to also work with sparse data
         X_data = X.data if sparse.issparse(X) else X
-        if np.nanmin(X_data) < 0:
-            raise ValueError("X needs to contain only non-negative integers.")
         X_data += 3
-        
         return X
 
     def fit_transform(self, X, y=None):
---
+++ test/test_pipeline/implementations/test_CategoryShift.py
@@ -0,0 +1,45 @@
+import unittest
+import numpy as np
+import scipy.sparse
+from sklearn.utils.testing import assert_array_almost_equal
+
+from autosklearn.pipeline.implementations.CategoryShift import CategoryShift
+
+
+class CategoryShiftTest(unittest.TestCase):
+
+    def test_dense(self):
+        X = np.random.randint(0, 255, (3,4))
+        X_copy = np.copy(X)
+        Y = CategoryShift().fit_transform(X_copy)
+        assert_array_almost_equal(X_copy, X + 3)
+        # Check if no copies were made
+        self.assertEqual(id(X_copy), id(Y))
+    
+    def test_sparse(self):
+        X = scipy.sparse.csc_matrix(([1, 2, 0, 4], ([0, 1, 2, 1], [3, 2, 1, 0])), shape=(3, 4))
+        X_copy = scipy.sparse.csc_matrix.copy(X)
+        Y = CategoryShift().fit_transform(X_copy)
+        X.data += 3
+        assert_array_almost_equal(X.todense(), X_copy.todense())
+        # Check if no copies were made
+        self.assertEqual(id(X_copy), id(Y))
+
+    def test_negative(self):
+        X = np.array([[-1,2], [3, 4]])
+        with self.assertRaises(ValueError):
+            CategoryShift().fit_transform(X)
+    
+    def test_string(self):
+        X = np.array([['blue', 'flat'], ['red', 'convex']])
+        with self.assertRaises(ValueError):
+            CategoryShift().fit_transform(X)
+
+    def test_fit_doesnt_modify_array(self):
+        X = np.random.rand(3,4) * 10.
+        X_copy = np.copy(X)
+        CategoryShift().fit(X)
+        assert_array_almost_equal(X, X_copy)
+
+
+        
