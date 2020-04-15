782453cc70853b1546e20fb4316fd33090f96b53
Gui Miotto <guilherme.miotto@gmail.com>
2020-04-15 17:31:28 +0200

    fix small bug

--- test/test_pipeline/components/dummy_components/dummy_component_import.py
+++ test/test_pipeline/components/dummy_components/dummy_component_import.py
@@ -1,3 +1,2 @@
-from autosklearn.pipeline.components.base import \
-    find_components,  # noqa
-    AutoSklearnClassificationAlgorithm,  # noqa
+from autosklearn.pipeline.components.base import find_components # noqa
+from autosklearn.pipeline.components.base import AutoSklearnClassificationAlgorithm  # noqa
