152a480094844353d93c20b9731fbce2d5ffc1e4
chico <francisco.rivera.valverde@gmail.com>
2020-08-12 16:00:47 +0200

    Improved comments

--- autosklearn/estimators.py
+++ autosklearn/estimators.py
@@ -669,6 +669,9 @@ class AutoSklearnClassifier(AutoSklearnEstimator):
         if target_type not in supported_types:
             raise ValueError("Classification with data of type {} is "
                              "not supported. Supported types are {}. "
+                             "You can find more information about scikit-learn "
+                             "data types in: "
+                             "https://scikit-learn.org/stable/modules/multiclass.html"
                              "".format(
                                     target_type,
                                     supported_types
@@ -797,13 +800,17 @@ class AutoSklearnRegressor(AutoSklearnEstimator):
         """
         # Before running anything else, first check that the
         # type of data is compatible with auto-sklearn. Legal target
-        # types are: continuous, continuous-multioutput, 'multiclass'
-        # binary
+        # types are: continuous, continuous-multioutput, and the special cases:
+        # multiclass : because [3.0, 1.0, 5.0] is considered as multiclass
+        # binary: because [1.0, 0.0] is considered multiclass
         target_type = type_of_target(y)
         supported_types = ['continuous', 'binary', 'multiclass', 'continuous-multioutput']
         if target_type not in supported_types:
             raise ValueError("Regression with data of type {} is "
                              "not supported. Supported types are {}. "
+                             "You can find more information about scikit-learn "
+                             "data types in: "
+                             "https://scikit-learn.org/stable/modules/multiclass.html"
                              "".format(
                                     target_type,
                                     supported_types
