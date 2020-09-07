baa7fd9c113602b1f247983db54fd7991b0fec15
charles <ccfu@kth.se>
2020-09-07 18:26:55 +0200

    Fix flake8 warning

--- autosklearn/evaluation/train_evaluator.py
+++ autosklearn/evaluation/train_evaluator.py
@@ -912,9 +912,9 @@ class TrainEvaluator(AbstractEvaluator):
                 ref_arg_dict = __baseCrossValidator_defaults__[class_name]
 
                 y = D.data['Y_train']
-                if (D.info['task'] in CLASSIFICATION_TASKS and \
+                if (D.info['task'] in CLASSIFICATION_TASKS and
                    D.info['task'] != MULTILABEL_CLASSIFICATION) or \
-                   (D.info['task'] in REGRESSION_TASKS and \
+                   (D.info['task'] in REGRESSION_TASKS and
                    D.info['task'] != MULTIOUTPUT_REGRESSION):
 
                     y = y.ravel()
