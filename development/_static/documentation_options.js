1023d88bee84f53f99005b0d763ef6d98f2d9ccc
JinWoo <deploy@travis-ci.org>
2020-09-20 11:38:24 +0000

    Deploy automl/auto-sklearn to github.com/automl/auto-sklearn.git:gh-pages

--- development/_static/documentation_options.js
+++ development/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0',
+    VERSION: '0.10.0dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
