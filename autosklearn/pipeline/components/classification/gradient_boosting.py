c3049c0bfc8cff5f5a2d3071779f9b332273bba0
ROHIT AGARWAL <rohit1049@gmail.com>
2020-11-11 14:33:45 +0530

    Upgrade scikit learn to 0.23.x (#984)
    * Update scikit-learn to 0.23.x
    
    * Fix: LDA classifier import missing
    
    * Incorporated review comments
    
    * Fixing code formatting
    
    * Incorporating review comments
    
    * Update base.py
    
    Co-authored-by: Rohit Agarwal <rohit.agarwal4@aexp.com>

--- autosklearn/pipeline/components/classification/gradient_boosting.py
+++ autosklearn/pipeline/components/classification/gradient_boosting.py
@@ -8,14 +8,13 @@ from ConfigSpace.conditions import EqualsCondition, InCondition
 
 from autosklearn.pipeline.components.base import (
     AutoSklearnClassificationAlgorithm,
-    IterativeComponent,
-)
+    IterativeComponentWithSampleWeight)
 from autosklearn.pipeline.constants import DENSE, UNSIGNED_DATA, PREDICTIONS
 from autosklearn.util.common import check_none
 
 
 class GradientBoostingClassifier(
-    IterativeComponent,
+    IterativeComponentWithSampleWeight,
     AutoSklearnClassificationAlgorithm
 ):
     def __init__(self, loss, learning_rate, min_samples_leaf, max_depth,
@@ -47,7 +46,7 @@ class GradientBoostingClassifier(
     def get_current_iter(self):
         return self.estimator.n_iter_
 
-    def iterative_fit(self, X, y, n_iter=2, refit=False):
+    def iterative_fit(self, X, y, n_iter=2, refit=False, sample_weight=None):
 
         """
         Set n_iter=2 for the same reason as for SGD
@@ -119,7 +118,7 @@ class GradientBoostingClassifier(
             self.estimator.max_iter = min(self.estimator.max_iter,
                                           self.max_iter)
 
-        self.estimator.fit(X, y)
+        self.estimator.fit(X, y, sample_weight=sample_weight)
 
         if self.estimator.max_iter >= self.max_iter \
            or self.estimator.max_iter > self.estimator.n_iter_:
@@ -174,10 +173,9 @@ class GradientBoostingClassifier(
         max_bins = Constant("max_bins", 255)
         l2_regularization = UniformFloatHyperparameter(
             name="l2_regularization", lower=1E-10, upper=1, default_value=1E-10, log=True)
-        # Train temporarily disabled to avoid error with scikit-learn 0.22
-        # TODO re-enable with later scikit-learn version
+
         early_stop = CategoricalHyperparameter(
-            name="early_stop", choices=["off", "valid"], default_value="off")
+            name="early_stop", choices=["off", "valid", "train"], default_value="off")
         tol = UnParametrizedHyperparameter(
             name="tol", value=1e-7)
         scoring = UnParametrizedHyperparameter(
@@ -193,7 +191,7 @@ class GradientBoostingClassifier(
                                 validation_fraction])
 
         n_iter_no_change_cond = InCondition(
-            n_iter_no_change, early_stop, ["valid"])  # , "train"])
+            n_iter_no_change, early_stop, ["valid", "train"])
         validation_fraction_cond = EqualsCondition(
             validation_fraction, early_stop, "valid")
 
