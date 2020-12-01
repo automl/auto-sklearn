53714646296ad409e12ecf0929f641969067fd30
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2020-12-01 10:07:21 +0100

    Test different pynisher version to fix 1016
    https://github.com/automl/pynisher/tree/askl1016

--- autosklearn/__init__.py
+++ autosklearn/__init__.py
@@ -3,14 +3,14 @@ import os
 import pkg_resources
 import sys
 
-from autosklearn.util import dependencies
+# from autosklearn.util import dependencies
 from autosklearn.__version__ import __version__  # noqa (imported but unused)
 
 
 requirements = pkg_resources.resource_string('autosklearn', 'requirements.txt')
 requirements = requirements.decode('utf-8')
 
-dependencies.verify_packages(requirements)
+# dependencies.verify_packages(requirements)
 
 if os.name != 'posix':
     raise ValueError(
