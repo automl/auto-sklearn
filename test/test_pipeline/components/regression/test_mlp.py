a4768c87c597980d7ae6486d68cfb3f3eec01cec
eggenspk <eggenspk@informatik.uni-freiburg.de>
2020-12-07 14:59:56 +0100

    ADD mlp for regression

--- test/test_pipeline/components/regression/test_mlp.py
+++ test/test_pipeline/components/regression/test_mlp.py
@@ -10,17 +10,19 @@ class MLPComponentTest(BaseRegressionComponentTest):
     __test__ = True
 
     res = dict()
-    res["default_boston"] = 0.5572566941424424
-    res["boston_n_calls"] = 9
-    res["default_boston_iterative"] = 0.3991011585607287
+    res["default_boston"] = 0.3622815479003514
+    res["boston_n_calls"] = 8
+    res["boston_iterative_n_iter"] = 161
+    res["default_boston_iterative"] = res["default_boston"]
     res["default_boston_iterative_places"] = 1
-    res["default_boston_sparse"] = -0.5023509031147615
-    res["default_boston_iterative_sparse"] = -0.49785984132922323
-    res["default_diabetes"] = 0.39632559079949414
+    res["default_boston_sparse"] = -0.4663396612593085
+    res["default_boston_iterative_sparse"] = res["default_boston_sparse"]
+    res["default_diabetes"] = 0.33660371480430984
     res["diabetes_n_calls"] = 9
-    res["default_diabetes_iterative"] = 0.4402964422631598
-    res["default_diabetes_sparse"] = 0.2283137551930371
-    res["default_diabetes_iterative_sparse"] = 0.27430032244279856
+    res["diabetes_iterative_n_iter"] = 106
+    res["default_diabetes_iterative"] = res["default_diabetes"]
+    res["default_diabetes_sparse"] = 0.033787581215897866
+    res["default_diabetes_iterative_sparse"] = res["default_diabetes_sparse"]
 
     sk_mod = sklearn.neural_network.MLPRegressor
     module = MLPRegressor
