0a2a44c64516ca95e1433b99c9b3236b53505d71
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2021-01-21 14:01:45 +0100

    Update parallel.py

--- autosklearn/util/parallel.py
+++ autosklearn/util/parallel.py
@@ -7,12 +7,12 @@ def preload_modules(context: multiprocessing.context.BaseContext) -> None:
     preload = [
         loaded_module for loaded_module in all_loaded_modules
         if loaded_module.split('.')[0] in (
-            'smac', 
-            'autosklearn', 
-            'numpy', 
-            'scipy', 
-            'pandas', 
-            'pynisher', 
+            'smac',
+            'autosklearn',
+            'numpy',
+            'scipy',
+            'pandas',
+            'pynisher',
             'sklearn',
             'ConfigSpace',
         ) and 'logging' not in loaded_module
