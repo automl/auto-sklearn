80a1ae54852cd29acba3179b4143517399666d52
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2021-02-17 11:56:40 +0100

    fix unit test

--- test/test_data/test_feature_validator.py
+++ test/test_data/test_feature_validator.py
@@ -35,11 +35,11 @@ def input_data_featuretest(request):
             ['a', 'b', 'd', 'r', 'b', 'c'],
         ])
     elif request.param == 'numpy_categoricalonly_nan':
-        array = np.random.randint(10, size=(100, 10))
+        array = np.random.randint(10, size=(100, 10)).astype('float')
         array[50, 0:5] = np.nan
         return array
     elif request.param == 'numpy_numericalonly_nan':
-        array = np.random.uniform(10, size=(100, 10))
+        array = np.random.uniform(10, size=(100, 10)).astype('float')
         array[50, 0:5] = np.nan
         return array
     elif request.param == 'numpy_mixed_nan':
