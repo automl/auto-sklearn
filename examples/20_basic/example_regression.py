47c4480a0e5c02dfaa5d44cfd3d85a46e4c732d7
eggenspk <eggenspk@informatik.uni-freiburg.de>
2021-03-26 17:29:54 +0100

    Update regression example

--- examples/20_basic/example_regression.py
+++ examples/20_basic/example_regression.py
@@ -11,13 +11,13 @@ import sklearn.datasets
 import sklearn.metrics
 
 import autosklearn.regression
-
+import matplotlib.pyplot as plt
 
 ############################################################################
 # Data Loading
 # ============
 
-X, y = sklearn.datasets.load_boston(return_X_y=True)
+X, y = sklearn.datasets.load_diabetes(return_X_y=True)
 
 X_train, X_test, y_train, y_test = \
     sklearn.model_selection.train_test_split(X, y, random_state=1)
@@ -32,7 +32,7 @@ automl = autosklearn.regression.AutoSklearnRegressor(
     tmp_folder='/tmp/autosklearn_regression_example_tmp',
     output_folder='/tmp/autosklearn_regression_example_out',
 )
-automl.fit(X_train, y_train, dataset_name='boston')
+automl.fit(X_train, y_train, dataset_name='diabetes')
 
 ############################################################################
 # Print the final ensemble constructed by auto-sklearn
@@ -44,5 +44,20 @@ print(automl.show_models())
 # Get the Score of the final ensemble
 # ===================================
 
-predictions = automl.predict(X_test)
-print("R2 score:", sklearn.metrics.r2_score(y_test, predictions))
+train_predictions = automl.predict(X_train)
+print("Train R2 score:", sklearn.metrics.r2_score(y_train, train_predictions))
+test_predictions = automl.predict(X_test)
+print("Test R2 score:", sklearn.metrics.r2_score(y_test, test_predictions))
+
+###########################################################################
+# Plot the predictions
+# ===================================
+plt.scatter(train_predictions, y_train, label="Train samples")
+plt.scatter(test_predictions, y_test, label="Test samples")
+plt.xlabel("Predictions")
+plt.ylabel("True values")
+plt.legend()
+plt.plot([40, 350], [40, 350], c='k', zorder=0)
+plt.xlim([40, 350])
+plt.ylim([40, 350])
+plt.show()
