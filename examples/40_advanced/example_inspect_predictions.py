c1d3363fff47d133b0b9e7cfc989301c7e1a1c8b
eggenspk <eggenspk@mllap06.informatik.privat>
2021-04-06 14:46:31 +0200

    ADD example on how to use sklearn's inspect module

---
+++ examples/40_advanced/example_inspect_predictions.py
@@ -0,0 +1,102 @@
+# -*- encoding: utf-8 -*-
+"""
+==============
+Generating PD and ICE plots
+==============
+
+The following example shows how to fit a simple classification model with
+*auto-sklearn* and use the inspect `module <https://scikit-learn.org/stable/inspection.html>`_ from
+scikit-learn to understand what affects the predictions.
+"""
+import sklearn.datasets
+from sklearn.inspection import plot_partial_dependence, permutation_importance
+import matplotlib.pyplot as plt
+import autosklearn.classification
+
+
+################################
+# Load Data and Build a Model
+# ==============================
+#
+# We use the Run or walk dataset as given by OpenML. The goal is to predict whether a person is
+# running or walking based on accelerometer and gyroscope data collected by a phone. For more
+# information see `here <https://www.openml.org/d/40922>`_.
+
+dataset = sklearn.datasets.fetch_openml(data_id=40922)
+
+# Note: To speed up the example, we subsample the dataset
+dataset.data = dataset.data.sample(n=5000, random_state=1, axis="index")
+dataset.target = dataset.target[dataset.data.index]
+X_train, X_test, y_train, y_test = \
+    sklearn.model_selection.train_test_split(dataset.data, dataset.target, test_size=0.3,
+                                             random_state=1)
+
+automl = autosklearn.classification.AutoSklearnClassifier(
+    time_left_for_this_task=360,
+    per_run_time_limit=30,
+    tmp_folder='/tmp/autosklearn_classification_example_tmp',
+    output_folder='/tmp/autosklearn_classification_example_out',
+    n_jobs=1,
+)
+automl.fit(X_train, y_train, dataset_name='Run_or_walk_information')
+
+s = automl.score(X_train, y_train)
+print(f"Train score {s}")
+s = automl.score(X_test, y_test)
+print(f"Test score {s}")
+
+#########################################
+# Compute permutation importance - part 1
+# =======================================
+#
+# Since auto-sklearn implements the scikit-learn interface, it can be used with the scikit-learn's
+# model inspection module. So, now we first look at the permutation importance, which defines the
+# increase in a model score when a given feature is randomly permutated. So, the higher the score,
+# the more does the model's predictions depend on this feature.
+#
+# **Note:** There are some pitfalls in interpreting these numbers, which can be found
+# `here <https://scikit-learn.org/stable/modules/permutation_importance.html>`_.
+
+r = permutation_importance(automl, X_test, y_test,
+                           n_repeats=10,
+                           random_state=0)
+
+for i in r.importances_mean.argsort()[::-1]:
+    print(f"{dataset.feature_names[i]:10s}: {r.importances_mean[i]:.3f} +/- "
+          f"{r.importances_std[i]:.3f}")
+
+############################################################################################
+# Create partial dependence (PD) and individual conditional expectation (ICE) plots - part 2
+# ==========================================================================================
+#
+# ICE plots plot the relation between feature values and the response value for each sample
+# individually -- it shows how the response value changes if the value of one feature is changed.
+#
+# PD plots plot the relation between feature values and the response value, i.e. the expected
+# response value wrt. one or multiple input features. Since we use a classification dataset, this
+# corresponds to the predicted class probability.
+#
+# Since acceleration_y and acceleration_z turned out to have the largest impact on the response
+# value according to the permutation dependence, we'll first look at them and generate a plot
+# combining ICE (thin lines) and PD (thick line)
+
+features = [1, 2]
+plot_partial_dependence(automl, dataset.data, features=features, grid_resolution=5,
+                        kind="both", feature_names=dataset.feature_names,
+                        )
+plt.show()
+
+##########################################################################
+# Create partial dependence (PDP) plots for more than one feature - part 3
+# ========================================================================
+#
+# PDP plot can also be generated for two features and thus allow to inspect the interaction between
+# these features. Again, we'll look at acceleration_y and acceleration_z.
+#
+# **Note:** This might take several minutes.
+
+features = [[1, 2]]
+plot_partial_dependence(automl, dataset.data, features=features, grid_resolution=5,
+                        feature_names=dataset.feature_names,
+                        )
+plt.show()
