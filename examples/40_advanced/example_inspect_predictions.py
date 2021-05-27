aef72de9eecfd7d20f06ce2df1df2665e51c47a7
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2021-05-27 13:23:42 +0200

    Update examples/40_advanced/example_inspect_predictions.py
    Co-authored-by: Matthias Feurer <feurerm@informatik.uni-freiburg.de>

--- examples/40_advanced/example_inspect_predictions.py
+++ examples/40_advanced/example_inspect_predictions.py
@@ -35,7 +35,7 @@ X_train, X_test, y_train, y_test = \
 automl = autosklearn.classification.AutoSklearnClassifier(
     time_left_for_this_task=120,
     per_run_time_limit=30,
-    tmp_folder='/tmp/autosklearn_classification_example_tmp',
+    tmp_folder='/tmp/autosklearn_inspect_predictions_example_tmp',
     output_folder='/tmp/autosklearn_classification_example_out',
     n_jobs=1,
 )
