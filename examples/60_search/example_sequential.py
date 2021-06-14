9ad7c4c6420ff9d1d353436fa0407cbf6e8a1786
chico <francisco.rivera.valverde@gmail.com>
2021-06-14 15:15:10 +0200

    Output folder is needed for ensemble output

--- examples/60_search/example_sequential.py
+++ examples/60_search/example_sequential.py
@@ -37,6 +37,7 @@ automl = autosklearn.classification.AutoSklearnClassifier(
     # finished fitting all machine learning models.
     ensemble_size=0,
     delete_tmp_folder_after_terminate=False,
+    delete_output_folder_after_terminate=False,
 )
 automl.fit(X_train, y_train, dataset_name='breast_cancer')
 
