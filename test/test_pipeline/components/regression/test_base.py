a47a4e4a571bc5e12c59ff6d58ebcf7f3f2e4904
eddiebergman <eddiebergmanhs@gmail.com>
2021-08-26 13:16:55 +0200

    Add module idempotent for regression pipelines

--- test/test_pipeline/components/regression/test_base.py
+++ test/test_pipeline/components/regression/test_base.py
@@ -1,3 +1,5 @@
+from typing import Dict, Optional, Tuple
+
 import unittest
 
 import numpy as np
@@ -210,3 +212,82 @@ class BaseRegressionComponentTest(unittest.TestCase):
                                                             predictions),
                                    places=self.res.get(
                                            "default_diabetes_sparse_places", 7))
+
+    def test_module_idempotent(self):
+        """ Fitting twice with the same config gives the same model params. """
+        if self.__class__ == BaseRegressionComponentTest:
+            return
+
+        regressor_cls = self.module
+
+        X = np.array([[0.5, 0.5], [0.0, 1.0], [1.0, 0.5], [1.0, 1.0]])
+        y = np.array([1, 1, 1, 1])
+
+        # There are certain errors we ignore so we wrap this in a function
+        def fitted_params(model) -> Dict:
+            """
+            Returns the params if fitted successfully, else None if an
+            acceptable error occurs
+
+            Currently we have no acceptable errors tracked for this regressors
+            """
+            model.fit(X.copy(), y.copy())
+            return model.estimator.get_params()
+
+        def random_state_keys(model) -> Optional[Tuple]:
+            """ Gets the keys used for random number generation """
+            if model.estimator is None:
+                return None  # An acceptable error occured in fitting
+            else:
+                return model.estimator.random_state.get_state()[1]
+
+        # We ignore certain keys when comparing
+        # Random state objects don't compare so we manually compare it's keys
+        param_keys_ignored = [
+            'random_state', 'base_estimator', *self.res.get('ignore_hps', [])
+        ]
+
+        # We use the default config + sampled ones
+        configuration_space = regressor_cls.get_hyperparameter_search_space()
+
+        default = configuration_space.get_default_configuration()
+        sampled = [configuration_space.sample_configuration() for _ in range(5)]
+
+        for config in [default] + sampled:
+            model_args = {
+                'random_state': np.random.RandomState(1),
+                ** {
+                    hp_name: config[hp_name]
+                    for hp_name in config
+                    if config[hp_name] is not None
+                }
+            }
+            regressor = regressor_cls(**model_args)
+
+            # Get the parameters on the first and second fit with config params
+            # We also ensure their random state has been correctly reset such
+            # that the state of their RandomState is the same after each fit call
+            params_first = fitted_params(regressor)
+            rand_state_keys_first = random_state_keys(regressor)
+
+            params_second = fitted_params(regressor)
+            rand_state_keys_second = random_state_keys(regressor)
+
+            # An acceptable error occured, skip to next sample
+            if params_first is None or params_second is None:
+                continue
+
+            # Remove keys we don't wish to include in the comparison
+            for params in [params_first, params_second]:
+                for key in param_keys_ignored:
+                    if key in params:
+                        del params[key]
+
+            # They should have equal parameters
+            self.assertEqual(params_first, params_second,
+                             f"Failed with model args {model_args}")
+
+            # Their random state keys (np.ndarray) should be the same
+            assert all(rand_state_keys_first == rand_state_keys_second), \
+                             f"The random states for {self.module} changed")
+
