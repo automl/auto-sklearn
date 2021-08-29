40b3051fa859bfd59749dce9d67e19d80e4fba57
Oli <ollihaus@t-online.de>
2021-08-29 14:31:16 +0200

    removed _ for :ref:

--- autosklearn/estimators.py
+++ autosklearn/estimators.py
@@ -235,7 +235,7 @@ class AutoSklearnEstimator(BaseEstimator):
         performance_over_time\_ : pandas.core.frame.DataFrame
             A ``DataFrame`` containing the models performance over time data. Can be
             used for plotting directly. Please refer to the example
-            :ref:`Train and Test Inputs <sphx_glr_examples_40_advanced_example_pandas_train_test.py>`_.
+            :ref:`Train and Test Inputs <sphx_glr_examples_40_advanced_example_pandas_train_test.py>`.
 
         """  # noqa (links are too long)
         # Raise error if the given total time budget is less than 30 seconds.
