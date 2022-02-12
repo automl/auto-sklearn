6fd3f61eafd437da8ad5913b3cfa419facd543bd
Basavasagar K Patil <59369344+bkpcoding@users.noreply.github.com>
2022-02-13 00:22:21 +0530

    Two tests for ensemble_size == 0 cases
    Added two tests to check if the automl.fit_ensemble() raises error when ensemble_size == 0 and if show_models() returns empty dictionary when ensemble_size == 0

--- test/test_automl/test_automl.py
+++ test/test_automl/test_automl.py
@@ -76,6 +76,32 @@ def test_fit(dask_client):
 
     del automl
 
+def test_ensemble_size_zero(self):
+    """ Test if automl.fit_ensemble raises error when ensemble_size == 0"""
+    X_train, Y_train, X_test, Y_test = putil.get_dataset('iris')
+    automl = autosklearn.automl.AutoML(
+        seed=0,
+        time_left_for_this_task=30,
+        per_run_time_limit=5,
+        metric=accuracy,
+        ensemble_size=0
+    )
+    automl.fit(X_train, Y_train, task=MULTICLASS_CLASSIFICATION)
+    with pytest.raises(ValueError):
+        automl.fit_ensemble(Y_test, ensemble_size=0)
+            
+def test_empty_dict_in_show_models(self):
+    """ Test if show_models() returns empty dictionary when ensemble_size == 0"""
+    X_train, Y_train, X_test, Y_test = putil.get_dataset('iris')
+    automl = autosklearn.automl.AutoMLClassifier(
+        seed=0,
+        time_left_for_this_task=30,
+        per_run_time_limit=5,
+        metric=accuracy,
+        ensemble_size=0
+    )
+    automl.fit(X_train, Y_train)
+    assert automl.show_models() == {}
 
 def test_fit_roar(dask_client_single_worker):
     def get_roar_object_callback(
