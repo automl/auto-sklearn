36d7dd6b31c6e746fcb36571ead5d142c99c5b8d
eddiebergman <eddiebergmanhs@gmail.com>
2022-03-28 00:04:09 +0200

    Add disk util file with `sizeof`

---
+++ autosklearn/util/disk.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+sizes = {
+    "B": 0,
+    "KB": 1,
+    "MB": 2,
+    "GB": 3,
+    "TB": 4,
+}
+
+
+def sizeof(path: Path | str, unit: str = "B") -> int:
+    """Get the size of some path object
+
+    Parameters
+    ----------
+    path : Path | str
+        The path of the file or directory to get the size of
+
+    unit : "B" | "KB" | "MB" | "GB" | "TB" = "B"
+        What unit to get the answer in
+
+    Returns
+    -------
+    int
+        The size of the folder/file in the given units
+    """
+    if unit not in sizes:
+        raise ValueError(f"Not a known unit {unit}")
+
+    if not isinstance(path, Path):
+        path = Path(path)
+
+    if path.is_file():
+        size = path.stat().st_size
+    else:
+        size = sum(f.stat().st_size for f in path.glob("**/*") if f.is_file())
+
+    power = sizes[unit]
+    return round(size / (1024**power))
