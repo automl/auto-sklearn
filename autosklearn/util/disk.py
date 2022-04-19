d938b0052ca90ad91c1634d57cb2b41b5eb92f76
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-19 22:59:43 +0200

    Add: atomic rmtree now accepts where mv should go

--- autosklearn/util/disk.py
+++ autosklearn/util/disk.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from typing import Any
 
 import math
-import uuid
 import shutil
+import tempfile
+import uuid
 from pathlib import Path
 
 sizes = {
@@ -47,7 +48,13 @@ def sizeof(path: Path | str, unit: str = "B") -> float:
     return size / math.pow(1024, power)
 
 
-def rmtree(path: Path | str, *, atomic: bool = False, **kwargs: Any) -> None:
+def rmtree(
+    path: Path | str,
+    *,
+    atomic: bool = False,
+    tmp: bool | Path | str = False,
+    **kwargs: Any,
+) -> None:
     """Delete a file or directory
 
     Parameters
@@ -56,29 +63,41 @@ def rmtree(path: Path | str, *, atomic: bool = False, **kwargs: Any) -> None:
         The path to delete
 
     atomic: bool = False
-        Whether to delete the file/folder atomically. This is done using
-        `move` and `rmtree`.
+        Whether to delete the file/folder atomically. This is done by first
+        using a `move` before `rmtree`.
 
-        The deletion part is not guaranteed to be atomic but the folder
-        is highly likely to at least be renamed.
-
-        The `move` is not guaranteed to be atomic either if moving between
+        The `move` is not guaranteed to be atomic if moving between
         different file systems which can happen when moving to /tmp,
         depending on the OS and setup.
 
+        The deletion part is not atomic.
+
         * https://docs.python.org/3/library/shutil.html#shutil.move
 
+    tmp: bool | Path | str = False
+        If bool, this defines whether atomic should use the tmp dir
+        for it's move. Otherwise, a path can be specified to use
+
     **kwargs
-        Forwarded to `rmtree` if `atmoic=True`
+        Forwarded to `rmtree`
         * https://docs.python.org/3/library/shutil.html#shutil.rmtree
     """
     if isinstance(path, str):
         path = Path(path)
 
     if atomic:
-        uid = uuid.uuid4()
-        mvpath = path.parent / f"{path.name}.old_{uid}"
+        if tmp is True:
+            dir = Path(tempfile.gettempdir())
+            uid = uuid.uuid4()
+            mvpath = dir / f"autosklearn-{path.name}.old_{uid}"
+
+        elif tmp is False:
+            uid = uuid.uuid4()
+            mvpath = path.parent / f"{path.name}.old_{uid}"
+        else:
+            mvpath = tmp if isinstance(tmp, Path) else Path(tmp)
+
         shutil.move(str(path), str(mvpath))
         shutil.rmtree(mvpath, **kwargs)
     else:
-        shutil.rmtree(mvpath, **kwargs)
+        shutil.rmtree(path, **kwargs)
