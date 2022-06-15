89295a3a6d51e77dd7ed4a0470640167d37ac129
eddiebergman <eddiebergmanhs@gmail.com>
2022-06-15 14:24:59 +0200

    Update random forest regressor test fixtures

--- test/test_pipeline/components/regression/test_random_forests.py
+++ test/test_pipeline/components/regression/test_random_forests.py
@@ -14,10 +14,10 @@ class RandomForestComponentTest(BaseRegressionComponentTest):
     res["default_boston_iterative"] = res["default_boston"]
     res["default_boston_sparse"] = 0.4194462097407078
     res["default_boston_iterative_sparse"] = res["default_boston_sparse"]
-    res["default_diabetes"] = 0.3496051170409269
+    res["default_diabetes"] = 0.35034589202040234
     res["diabetes_n_calls"] = 9
     res["default_diabetes_iterative"] = res["default_diabetes"]
-    res["default_diabetes_sparse"] = 0.2383300978781976
+    res["default_diabetes_sparse"] = 0.23731783691227226
     res["default_diabetes_iterative_sparse"] = res["default_diabetes_sparse"]
 
     sk_mod = sklearn.ensemble.RandomForestRegressor
