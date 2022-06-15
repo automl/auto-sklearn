10b535ecfe575e8a7138a9b5e4d9c6eaa31c791b
eddiebergman <eddiebergmanhs@gmail.com>
2022-06-15 14:27:05 +0200

    Update SVM regression test fixtures

--- test/test_pipeline/components/regression/test_support_vector_regression.py
+++ test/test_pipeline/components/regression/test_support_vector_regression.py
@@ -13,9 +13,9 @@ class SupportVectorComponentTest(BaseRegressionComponentTest):
     res["default_boston_iterative"] = None
     res["default_boston_sparse"] = -0.062749211736050192
     res["default_boston_iterative_sparse"] = None
-    res["default_diabetes"] = 0.12849591861430087
+    res["default_diabetes"] = 0.1284951797452738
     res["default_diabetes_iterative"] = None
-    res["default_diabetes_sparse"] = 0.0098877566961463881
+    res["default_diabetes_sparse"] = 0.009887486280726332
     res["default_diabetes_iterative_sparse"] = None
 
     sk_mod = sklearn.svm.SVR
